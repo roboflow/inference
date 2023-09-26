429f165df76ced56f6f433537fb223423fe6cddb
Paul Guerrie <paul@roboflow.com>
2023-09-26 17:29:43 +0000

    Fixed but in new auto orient logic

--- inference/core/utils/image_utils.py
+++ inference/core/utils/image_utils.py
@@ -66,8 +66,10 @@ def load_image(value: Any, disable_preproc_auto_orient=False) -> np.ndarray:
     return np_image, is_bgr
 
 
-def load_image_rgb(value: Any, cv_imread_flags=[]) -> np.ndarray:
-    np_image, is_bgr = load_image(value, cv_imread_flags=cv_imread_flags)
+def load_image_rgb(value: Any, disable_preproc_auto_orient=False) -> np.ndarray:
+    np_image, is_bgr = load_image(
+        value, disable_preproc_auto_orient=disable_preproc_auto_orient
+    )
     if is_bgr:
         np_image = cv2.cvtColor(np_image, cv2.COLOR_BGR2RGB)
     return np_image
