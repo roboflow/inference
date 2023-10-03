b0325ba37c26f16c13025802906aeade3fcadf7f
SolomonLake <lakegh@gmail.com>
2023-10-03 14:07:03 -0500

    Update __version__ import sorting for make style

--- inference/core/interfaces/udp/udp_stream.py
+++ inference/core/interfaces/udp/udp_stream.py
@@ -26,8 +26,8 @@ from inference.core.env import (
 )
 from inference.core.interfaces.base import BaseInterface
 from inference.core.interfaces.camera.camera import WebcamStream
-from version import __version__
 from inference.models.utils import get_roboflow_model
+from version import __version__
 
 
 class UdpStream(BaseInterface):
