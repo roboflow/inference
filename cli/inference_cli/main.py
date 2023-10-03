e60fe235b39ff87cb3eed05b91edad0ceb520955
SolomonLake <lakegh@gmail.com>
2023-10-03 13:59:03 -0500

    Update basic inference CLI options

--- cli/inference_cli/main.py
+++ cli/inference_cli/main.py
@@ -1,5 +1,4 @@
 import typer
-from typing_extensions import Annotated
 import inference_cli.actions
 from inference_cli.lib import start_inference_container
 
@@ -14,40 +13,37 @@ def serve(
         "--port",
         help="Port to run the inference server on (default is 9001).",
     ),
-    project: str = typer.Option(
+    rf_env: str = typer.Option(
         "roboflow-platform",
-        "-pr",
-        "--project",
-        help="Project to run the inference server with (default is roboflow-platform).",
+        "-rfe",
+        "--rf-env",
+        help="Roboflow environment to run the inference server with (default is roboflow-platform).",
     ),
 ):
-    """Start the inference server."""
-    print(f"starting inference server on port {port}")
-    start_inference_container("", port=port, project=project)
+    start_inference_container("", port=port, project=rf_env)
 
 
 @app.command()
 def infer(
     image: str = typer.Option(
-        None, "-i", "--image", help="URL of image to run inference on."
+        None, "-i", "--image", help="URL or local path of image to run inference on."
     ),
     project_id: str = typer.Option(
-        None, "-p", "--project_id", help="Project to run inference with."
+        None, "-p", "--project-id", help="Roboflow project to run inference with."
     ),
     model_version: str = typer.Option(
-        None, "-v", "--model_version", help="Version of model to run inference with."
+        None, "-v", "--model-version", help="Version of model to run inference with."
     ),
     api_key: str = typer.Option(
-        None, "-a", "--api_key", help="Path to save output image to."
+        None, "-a", "--api-key", help="Roboflow API key for your workspace."
     ),
     host: str = typer.Option(
         "http://localhost:9001", "-h", "--host", help="Host to run inference on."
     ),
 ):
     typer.echo(
-        f"Running inference with image {image}, project {project_id}, version {model_version}, API key {api_key}, and host {host}"
+        f"Running inference on image {image}, using model ({project_id}/{model_version}), and host ({host})"
     )
-
     inference_cli.actions.infer(image, project_id, model_version, api_key, host)
 
 
