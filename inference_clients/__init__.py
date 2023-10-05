5bcfbe509bea8f41c5d29e90394b723ff9d08166
Paweł Pęczek <pawel@roboflow.com>
2023-10-05 19:59:47 +0200

    Fix CI

--- inference_clients/__init__.py
+++ inference_clients/__init__.py
@@ -1,7 +1,6 @@
-from inference_clients.version import __version__
-
 from inference_clients.http.client import InferenceHTTPClient
 from inference_clients.http.entities import (
     InferenceConfiguration,
     VisualisationResponseFormat,
 )
+from inference_clients.version import __version__
