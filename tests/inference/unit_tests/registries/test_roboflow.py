771a850e7c3dabd09a6ecb5a0dabe0f8552aa88b
Paweł Pęczek <pawel@roboflow.com>
2023-10-17 14:53:06 +0200

    Add tests for get_model_type()

--- tests/inference/unit_tests/registries/test_roboflow.py
+++ tests/inference/unit_tests/registries/test_roboflow.py
@@ -50,11 +50,11 @@ def test_get_model_id_chunks_when_valid_input_given() -> None:
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_get_model_metadata_from_cache_when_metadata_file_does_not_exist(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
-    construct_model_type_cache_path.return_value = os.path.join(
+    construct_model_type_cache_path_mock.return_value = os.path.join(
         empty_local_dir, "model_type.json"
     )
 
@@ -67,12 +67,12 @@ def test_get_model_metadata_from_cache_when_metadata_file_does_not_exist(
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_get_model_metadata_from_cache_when_metadata_file_is_not_json(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
     metadata_path = os.path.join(empty_local_dir, "model_type.json")
-    construct_model_type_cache_path.return_value = metadata_path
+    construct_model_type_cache_path_mock.return_value = metadata_path
     with open(metadata_path, "w") as f:
         f.write("FOR SURE NOT JSON :)")
 
@@ -85,12 +85,12 @@ def test_get_model_metadata_from_cache_when_metadata_file_is_not_json(
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_get_model_metadata_from_cache_when_metadata_file_is_empty(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
     metadata_path = os.path.join(empty_local_dir, "model_type.json")
-    construct_model_type_cache_path.return_value = metadata_path
+    construct_model_type_cache_path_mock.return_value = metadata_path
     with open(metadata_path, "w") as f:
         f.write("")
 
@@ -103,12 +103,12 @@ def test_get_model_metadata_from_cache_when_metadata_file_is_empty(
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_get_model_metadata_from_cache_when_metadata_is_invalid(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
     metadata_path = os.path.join(empty_local_dir, "model_type.json")
-    construct_model_type_cache_path.return_value = metadata_path
+    construct_model_type_cache_path_mock.return_value = metadata_path
     with open(metadata_path, "w") as f:
         f.write(json.dumps({"some": "key"}))
 
@@ -121,12 +121,12 @@ def test_get_model_metadata_from_cache_when_metadata_is_invalid(
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_get_model_metadata_from_cache_when_metadata_invalid(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
     metadata_path = os.path.join(empty_local_dir, "model_type.json")
-    construct_model_type_cache_path.return_value = metadata_path
+    construct_model_type_cache_path_mock.return_value = metadata_path
     with open(metadata_path, "w") as f:
         f.write(
             json.dumps(
@@ -186,12 +186,12 @@ def test_model_metadata_content_is_invalid_when_task_type_is_missing() -> None:
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_save_model_metadata_in_cache(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
     metadata_path = os.path.join(empty_local_dir, "model_type.json")
-    construct_model_type_cache_path.return_value = metadata_path
+    construct_model_type_cache_path_mock.return_value = metadata_path
 
     # when
     save_model_metadata_in_cache(
@@ -206,7 +206,7 @@ def test_save_model_metadata_in_cache(
     # then
     assert result["model_type"] == "yolov8l"
     assert result["project_task_type"] == "instance-segmentation"
-    construct_model_type_cache_path.assert_called_once_with(
+    construct_model_type_cache_path_mock.assert_called_once_with(
         dataset_id="some", version_id="1"
     )
 
@@ -605,12 +605,12 @@ def test_get_roboflow_model_type_when_response_is_valid(
 
 @mock.patch.object(roboflow, "construct_model_type_cache_path")
 def test_get_model_type_when_cache_is_utilised(
-    construct_model_type_cache_path: MagicMock,
+    construct_model_type_cache_path_mock: MagicMock,
     empty_local_dir: str,
 ) -> None:
     # given
     metadata_path = os.path.join(empty_local_dir, "model_type.json")
-    construct_model_type_cache_path.return_value = metadata_path
+    construct_model_type_cache_path_mock.return_value = metadata_path
     with open(metadata_path, "w") as f:
         f.write(
             json.dumps(
@@ -625,7 +625,7 @@ def test_get_model_type_when_cache_is_utilised(
     result = get_model_type(model_id="some/1", api_key="my_api_key")
 
     # then
-    construct_model_type_cache_path.assert_called_once_with(
+    construct_model_type_cache_path_mock.assert_called_once_with(
         dataset_id="some", version_id="1"
     )
     assert result == ("object-detection", "yolov8n")
@@ -648,3 +648,48 @@ def test_get_model_type_when_generic_model_is_utilised(
 
     # then
     assert result == expected_result
+
+
+@mock.patch.object(roboflow, "get_roboflow_workspace")
+@mock.patch.object(roboflow, "get_roboflow_dataset_type")
+@mock.patch.object(roboflow, "get_roboflow_model_type")
+@mock.patch.object(roboflow, "construct_model_type_cache_path")
+def test_get_model_type_when_roboflow_api_is_called(
+    construct_model_type_cache_path_mock: MagicMock,
+    get_roboflow_model_type_mock: MagicMock,
+    get_roboflow_dataset_type_mock: MagicMock,
+    get_roboflow_workspace_mock: MagicMock,
+    empty_local_dir: str,
+) -> None:
+    # given
+    metadata_path = os.path.join(empty_local_dir, "model_type.json")
+    construct_model_type_cache_path_mock.return_value = metadata_path
+    get_roboflow_model_type_mock.return_value = "yolov8n"
+    get_roboflow_dataset_type_mock.return_value = "object-detection"
+    get_roboflow_workspace_mock.return_value = "my_workspace"
+
+    # when
+    result = get_model_type(
+        model_id="some/1",
+        api_key="my_api_key",
+    )
+
+    # then
+    assert result == ("object-detection", "yolov8n")
+    with open(metadata_path) as f:
+        persisted_metadata = json.load(f)
+    assert persisted_metadata["model_type"] == "yolov8n"
+    assert persisted_metadata["project_task_type"] == "object-detection"
+    get_roboflow_model_type_mock.assert_called_once_with(
+        api_key="my_api_key",
+        workspace_id="my_workspace",
+        dataset_id="some",
+        version_id="1",
+        project_task_type="object-detection",
+    )
+    get_roboflow_dataset_type_mock.assert_called_once_with(
+        api_key="my_api_key",
+        workspace_id="my_workspace",
+        dataset_id="some",
+    )
+    get_roboflow_workspace_mock.assert_called_once_with(api_key="my_api_key")
