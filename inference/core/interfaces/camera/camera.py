e12e7576ab72278118be1aa2e30484e3e1bd8ad7
Sam Beran <sberan@gmail.com>
2023-10-23 11:33:19 -0500

    skip to latest frame of camera stream by default

--- inference/core/interfaces/camera/camera.py
+++ inference/core/interfaces/camera/camera.py
@@ -36,6 +36,12 @@ class WebcamStream:
         self.vcap = cv2.VideoCapture(self.stream_id)
         self.width = int(self.vcap.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.file_mode = self.vcap.get(cv2.CAP_PROP_FRAME_COUNT) > 0
+        if self.enforce_fps and not self.file_mode:
+            logger.warn(
+                "Ignoring enforce_fps flag for this stream. It is not compatible with streams and will cause the process to crash"
+            )
+            self.enforce_fps = False
         self.max_fps = 30
         if self.vcap.isOpened() is False:
             logger.debug("[Exiting]: Error accessing webcam stream.")
@@ -61,8 +67,7 @@ class WebcamStream:
     def update(self):
         """Update the frame by reading from the webcam."""
         frame_id = 0
-        skip_seconds = 0
-        last_frame_position = time.perf_counter()
+        next_frame_time = 0
         t0 = time.perf_counter()
         while True:
             t1 = time.perf_counter()
@@ -70,36 +75,28 @@ class WebcamStream:
                 break
 
             self.grabbed = self.vcap.grab()
-            if self.grabbed:
-                frame_id += 1
-                if (
-                    self.enforce_fps != "skip"
-                    or t1 >= last_frame_position + skip_seconds
-                ):
-                    ret, frame = self.vcap.retrieve()
-                    logger.debug("video capture FPS: %s", frame_id / (t1 - t0))
-                    if frame is not None:
-                        last_frame_position = t1
-                        self.frame_id = frame_id
-                        self.frame = frame
-                    else:
-                        logger.debug("[Exiting] Frame not available to retrieve")
-                        self.stopped = True
-                        break
-
             if self.grabbed is False:
                 logger.debug("[Exiting] No more frames to read")
                 self.stopped = True
                 break
-            if self.enforce_fps:
+            frame_id += 1
+            # We can't retrieve each frame on nano and other lower powered devices quickly enough to keep up with the stream.
+            # By default, we will only retrieve frames when we'll be ready process them (determined by self.max_fps).
+            if t1 > next_frame_time or self.enforce_fps:
+                ret, frame = self.vcap.retrieve()
+                if frame is None:
+                    logger.debug("[Exiting] Frame not available for read")
+                    self.stopped = True
+                    break
+                logger.debug(f"video capture effective FPS: {frame_id / (t1 - t0):.2f}")
+                self.frame_id = frame_id
+                self.frame = frame
+                next_frame_time = t1 + (1 / self.max_fps) + 0.02
+            if self.file_mode:
                 t2 = time.perf_counter()
-                next_frame = max(
-                    1 / self.max_fps + 0.02, 1 / self.fps_input_stream - (t2 - t1)
-                )
-                if self.enforce_fps == "skip":
-                    skip_seconds = next_frame
-                else:
-                    time.sleep(next_frame)
+                time_to_sleep = (1 / self.fps_input_stream) - (t2 - t1)
+                if time_to_sleep > 0:
+                    time.sleep(time_to_sleep)
         self.vcap.release()
 
     def read_opencv(self):
--- inference/core/interfaces/stream/stream.py
+++ inference/core/interfaces/stream/stream.py
@@ -70,6 +70,7 @@ class Stream(BaseInterface):
         on_prediction: Callable = None,
         on_start: Callable = None,
         on_stop: Callable = None,
+        simulate_process_delay: float = 0,
     ):
         """Initialize the stream with the given parameters.
         Prints the server settings and initializes the inference with a test frame.
@@ -108,6 +109,7 @@ class Stream(BaseInterface):
         self.json_response = json_response
         self.use_main_thread = use_main_thread
         self.output_channel_order = output_channel_order
+        self.simulate_process_delay = simulate_process_delay
 
         self.inference_request_type = (
             inference.core.entities.requests.inference.ObjectDetectionInferenceRequest
@@ -246,6 +248,9 @@ class Stream(BaseInterface):
                     max_candidates=self.max_candidates,
                     max_detections=self.max_detections,
                 )
+
+                if self.simulate_process_delay > 0:
+                    time.sleep(self.simulate_process_delay)
 
                 if self.json_response:
                     predictions = self.model.make_response(
