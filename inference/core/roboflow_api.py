5135b95bf9218159336b676de765edd7a037d71f
Paweł Pęczek <pawel@roboflow.com>
2023-11-03 14:39:57 +0100

    Added dummy api response for new strategies

--- inference/core/roboflow_api.py
+++ inference/core/roboflow_api.py
@@ -220,7 +220,32 @@ def get_roboflow_active_learning_configuration(
                     {"type": "hourly", "value": 100},
                     {"type": "daily", "value": 1000},
                 ],
-            }
+            },
+            {
+                "name": "hard_examples",
+                "type": "close_to_threshold",
+                "threshold": 0.3,
+                "epsilon": 0.3,
+                "probability": 1.0,
+                "tags": ["hard_case"],
+                "limits": [
+                    {"type": "minutely", "value": 10},
+                    {"type": "hourly", "value": 100},
+                    {"type": "daily", "value": 1000},
+                ],
+            },
+            {
+                "name": "multiple_detections",
+                "type": "detections_number_based",
+                "probability": 1.0,
+                "more_than": 3,
+                "tags": ["crowded"],
+                "limits": [
+                    {"type": "minutely", "value": 10},
+                    {"type": "hourly", "value": 100},
+                    {"type": "daily", "value": 1000},
+                ],
+            },
         ],
         "batching_strategy": {
             "batches_name_prefix": "al_batch",
