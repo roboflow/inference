cb944a14354753f355ecffd39f9af741ccb2fbe4
Paweł Pęczek <pawel@roboflow.com>
2023-11-22 19:17:22 +0100

    Add tests for get_video_frames_generator()

--- tests/inference/unit_tests/core/interfaces/test_utils.py
+++ tests/inference/unit_tests/core/interfaces/test_utils.py
@@ -1,7 +1,9 @@
 import time
-from typing import Generator, Optional
+from typing import Generator, Optional, List
 from unittest import mock
 from unittest.mock import MagicMock
+
+import numpy as np
 
 from inference.core.interfaces.camera.utils import RateLimiter, limit_frame_rate, FPSLimiterStrategy, \
     resolve_limiter_strategy, get_video_frames_generator
@@ -68,14 +70,19 @@ def test_limit_frame_rate_when_frames_to_be_dropped_and_stream_is_to_fast() -> N
     frames_generator = generate_with_delay(items=10, delay=0.005)
 
     # when
-    result = list(limit_frame_rate(
+    results, results_timestamp = [], []
+    for result in limit_frame_rate(
         frames_generator=frames_generator,
         max_fps=100,
         strategy=FPSLimiterStrategy.DROP
-    ))
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
-    assert 0 < len(result) <= 5
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.01 for diff in timestamp_differences)
+    assert 0 < len(results) <= 5
 
 
 def test_limit_frame_rate_when_frames_to_be_dropped_and_stream_is_to_slow() -> None:
@@ -83,14 +90,19 @@ def test_limit_frame_rate_when_frames_to_be_dropped_and_stream_is_to_slow() -> N
     frames_generator = generate_with_delay(items=10, delay=0.02)
 
     # when
-    result = list(limit_frame_rate(
-        frames_generator=frames_generator,
-        max_fps=100,
-        strategy=FPSLimiterStrategy.DROP
-    ))
+    results, results_timestamp = [], []
+    for result in limit_frame_rate(
+            frames_generator=frames_generator,
+            max_fps=100,
+            strategy=FPSLimiterStrategy.DROP
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.01 for diff in timestamp_differences)
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def test_limit_frame_rate_when_frames_to_be_awaited_and_stream_is_to_fast() -> None:
@@ -98,14 +110,19 @@ def test_limit_frame_rate_when_frames_to_be_awaited_and_stream_is_to_fast() -> N
     frames_generator = generate_with_delay(items=10, delay=0.005)
 
     # when
-    result = list(limit_frame_rate(
-        frames_generator=frames_generator,
-        max_fps=100,
-        strategy=FPSLimiterStrategy.WAIT
-    ))
+    results, results_timestamp = [], []
+    for result in limit_frame_rate(
+            frames_generator=frames_generator,
+            max_fps=100,
+            strategy=FPSLimiterStrategy.WAIT,
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.01 for diff in timestamp_differences)
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def test_limit_frame_rate_when_frames_to_be_awaited_and_stream_is_to_slow() -> None:
@@ -113,14 +130,19 @@ def test_limit_frame_rate_when_frames_to_be_awaited_and_stream_is_to_slow() -> N
     frames_generator = generate_with_delay(items=10, delay=0.02)
 
     # when
-    result = list(limit_frame_rate(
-        frames_generator=frames_generator,
-        max_fps=100,
-        strategy=FPSLimiterStrategy.WAIT
-    ))
+    results, results_timestamp = [], []
+    for result in limit_frame_rate(
+            frames_generator=frames_generator,
+            max_fps=100,
+            strategy=FPSLimiterStrategy.WAIT,
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.01 for diff in timestamp_differences)
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def test_resolve_limiter_strategy_when_strategy_defined_explicitly() -> None:
@@ -180,6 +202,7 @@ def test_get_video_frames_generator_when_fps_modulation_disabled(init_mock: Magi
     init_mock.return_value = dummy_source
 
     # when
+
     result = list(get_video_frames_generator(stream="source-ref"))
 
     # then
@@ -190,43 +213,69 @@ def test_get_video_frames_generator_when_fps_modulation_disabled(init_mock: Magi
 @mock.patch.object(utils.VideoSource, "init")
 def test_get_video_frames_generator_when_fps_modulation_enabled_against_video_file(init_mock: MagicMock) -> None:
     # given
-    dummy_source = DummyVideoSource(items=10, delay=0.01)
+    source_properties = SourceProperties(width=100, height=100, total_frames=10, is_file=True, fps=25)
+    dummy_source = DummyVideoSource(items=10, delay=0.01, source_properties=source_properties)
     init_mock.return_value = dummy_source
 
     # when
-    result = list(get_video_frames_generator(stream="source-ref"))
+    results, results_timestamp = [], []
+    for result in get_video_frames_generator(
+        stream="source-ref",
+        max_fps=50,
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.02 for diff in timestamp_differences)
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert dummy_source.start_called is True
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 @mock.patch.object(utils.VideoSource, "init")
 def test_get_video_frames_generator_when_fps_modulation_enabled_against_fast_stream(init_mock: MagicMock) -> None:
     # given
-    dummy_source = DummyVideoSource(items=10, delay=0.01)
+    source_properties = SourceProperties(width=100, height=100, total_frames=-1, is_file=False, fps=25)
+    dummy_source = DummyVideoSource(items=10, delay=0.01, source_properties=source_properties)
     init_mock.return_value = dummy_source
 
     # when
-    result = list(get_video_frames_generator(stream="source-ref"))
+    results, results_timestamp = [], []
+    for result in get_video_frames_generator(
+            stream="source-ref",
+            max_fps=50,
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.02 for diff in timestamp_differences)
+    assert 0 <= len(results) <= 5
     assert dummy_source.start_called is True
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 @mock.patch.object(utils.VideoSource, "init")
 def test_get_video_frames_generator_when_fps_modulation_enabled_against_slow_stream(init_mock: MagicMock) -> None:
     # given
-    dummy_source = DummyVideoSource(items=10, delay=0.01)
+    dummy_source = DummyVideoSource(items=10, delay=0.02)
     init_mock.return_value = dummy_source
 
     # when
-    result = list(get_video_frames_generator(stream="source-ref"))
+    results, results_timestamp = [], []
+    for result in get_video_frames_generator(
+            stream="source-ref",
+            max_fps=200,
+    ):
+        results_timestamp.append(time.monotonic())
+        results.append(result)
 
     # then
+    timestamp_differences = get_differences(results_timestamp)
+    assert all(diff >= 0.02 for diff in timestamp_differences)
+    assert results == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert dummy_source.start_called is True
-    assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 class DummyVideoSource:
@@ -255,3 +304,35 @@ def generate_with_delay(items: int, delay: float) -> Generator[int, None, None]:
     for i in range(items):
         yield i
         time.sleep(delay)
+
+
+def get_differences(values: List[float]) -> List[float]:
+    result = []
+    for x, y in zip(values, values[1:]):
+        result.append(y - x)
+    return result
+
+
+def test_get_differences_when_empty_input_given() -> None:
+    # when
+    result = get_differences(values=[])
+
+    # then
+    assert len(result) == 0
+
+
+def test_get_differences_when_single_element_input_given() -> None:
+    # when
+    result = get_differences(values=[1.0])
+
+    # then
+    assert len(result) == 0
+
+
+def test_get_differences_when_multi_elements_input_given() -> None:
+    # when
+    result = get_differences(values=[1.0, 1.5, 2.5, 4])
+
+    # then
+    assert len(result) == 3
+    assert np.allclose(result, [0.5, 1.0, 1.5])
