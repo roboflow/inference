dae52598b3840e18040ae2fee3644bc4e276de47
Paweł Pęczek <pawel@roboflow.com>
2023-11-23 14:28:33 +0100

    Extend camera test demo

--- development/stream_interface/camera_test.py
+++ development/stream_interface/camera_test.py
@@ -1,18 +1,39 @@
+import argparse
+from typing import Union
+
+import supervision as sv
 import cv2
 
+from inference.core.utils.preprocess import letterbox_image
 
-def main() -> None:
-    stream = cv2.VideoCapture(0)
+
+def main(video: Union[str, int], display: int) -> None:
+    stream = cv2.VideoCapture(video)
+    fps_monitor = sv.FPSMonitor()
     while stream.isOpened():
-        status, image = stream.read()
+        status = stream.grab()
+        fps_monitor.tick()
+        fps_value = fps_monitor()
+        print("GRABBING FPS: ", fps_value)
         if not status:
+            print("EOS")
             break
-        cv2.imshow("stream", image)
-        key = cv2.waitKey(1)
-        if key == ord("q"):
-            break
+        if display > 0:
+            status, image = stream.retrieve()
+            if not status:
+                print("EOS")
+                break
+            image = letterbox_image(image=image, desired_size=(1280, 720))
+            cv2.imshow("stream", image)
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                break
     print("DONE")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("Camera test script")
+    parser.add_argument("--video", required=False, default=0)
+    parser.add_argument("--display", type=int, required=False, default=1)
+    args = parser.parse_args()
+    main(video=args.video, display=args.display)
