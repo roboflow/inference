61ea052ccf56df0bca25387c7125841e7ad51a4e
SolomonLake <lakegh@gmail.com>
2023-12-07 11:46:11 -0600

    Added keypoints test case and added better syntax

--- tests/inference/unit_tests/core/models/utils/test_keypoints.py
+++ tests/inference/unit_tests/core/models/utils/test_keypoints.py
@@ -3,7 +3,8 @@ from inference.core.models.utils.keypoints import (
 )
 
 
-def test_superset_keypoints_count():
+def test_superset_keypoints_count() -> None:
+    # given
     keypoints_metadata = {
         0: {
             0: "nose",
@@ -25,11 +26,14 @@ def test_superset_keypoints_count():
             16: "right_ankle",
         }
     }
+    # when
     keypoints_count = superset_keypoints_count(keypoints_metadata)
+    # then
     assert keypoints_count == 17
 
 
-def test_superset_keypoints_count_with_two_classes():
+def test_superset_keypoints_count_with_two_classes() -> None:
+    # given
     keypoints_metadata = {
         0: {
             0: "nose",
@@ -56,5 +60,38 @@ def test_superset_keypoints_count_with_two_classes():
             12: "right_hip",
         },
     }
+    # when
     keypoints_count = superset_keypoints_count(keypoints_metadata)
+    # then
     assert keypoints_count == 13
+
+
+def test_superset_keypoints_count_with_two_non_overlapping_classes() -> None:
+    # given
+    keypoints_metadata = {
+        0: {
+            0: "nose",
+            1: "left_eye",
+            2: "right_eye",
+            3: "left_ear",
+            4: "right_ear",
+            5: "left_shoulder",
+            6: "right_shoulder",
+        },
+        1: {
+            0: "nose1",
+            1: "left_eye1",
+            2: "right_eye2",
+            3: "left_ear3",
+            4: "right_ear4",
+            5: "left_shoulder5",
+            6: "right_shoulder6",
+            7: "left_elbow7",
+            8: "right_elbow8",
+            9: "left_wrist9",
+        },
+    }
+    # when
+    keypoints_count = superset_keypoints_count(keypoints_metadata)
+    # then
+    assert keypoints_count == 10
