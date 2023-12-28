c92bc95fefa0ae2356197c7572e78413800bf396
James Gallagher <jamesg@jamesg.blog>
2023-12-28 10:44:52 +0000

    run black and isort

--- tests/inference/integration_tests/groundingdino_test.py
+++ tests/inference/integration_tests/groundingdino_test.py
@@ -1,10 +1,11 @@
 import json
 import os
-import requests
+import time
 from copy import deepcopy
 from pathlib import Path
+
 import pytest
-import time
+import requests
 
 api_key = os.environ.get("API_KEY")
 port = os.environ.get("PORT", 9001)
@@ -22,7 +23,113 @@ TESTS = [
             },
             "text": ["apple"],
         },
-        "expected_response": {"visualization": None, "frame_id": None, "time": 4.5613939999602735, "image": {"width": 950, "height": 796}, "predictions": [{"x": 425.0, "y": 358.0, "width": 282.0, "height": 250.0, "confidence": 0.7121592164039612, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 612.0, "y": 113.0, "width": 253.0, "height": 207.0, "confidence": 0.7059137225151062, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 298.0, "y": 154.0, "width": 313.0, "height": 202.0, "confidence": 0.6812718510627747, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 397.0, "y": 186.0, "width": 179.0, "height": 142.0, "confidence": 0.6505898833274841, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 587.0, "y": 207.0, "width": 215.0, "height": 249.0, "confidence": 0.6408542394638062, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 221.0, "y": 333.0, "width": 232.0, "height": 242.0, "confidence": 0.675274670124054, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 663.0, "y": 390.0, "width": 193.0, "height": 182.0, "confidence": 0.6492464542388916, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 798.0, "y": 317.0, "width": 237.0, "height": 265.0, "confidence": 0.6517468690872192, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}, {"x": 234.0, "y": 637.0, "width": 216.0, "height": 286.0, "confidence": 0.5249432325363159, "class_name": "apple", "class_confidence": None, "class_id": 0, "tracker_id": None}]}
+        "expected_response": {
+            "visualization": None,
+            "frame_id": None,
+            "time": 4.5613939999602735,
+            "image": {"width": 950, "height": 796},
+            "predictions": [
+                {
+                    "x": 425.0,
+                    "y": 358.0,
+                    "width": 282.0,
+                    "height": 250.0,
+                    "confidence": 0.7121592164039612,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 612.0,
+                    "y": 113.0,
+                    "width": 253.0,
+                    "height": 207.0,
+                    "confidence": 0.7059137225151062,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 298.0,
+                    "y": 154.0,
+                    "width": 313.0,
+                    "height": 202.0,
+                    "confidence": 0.6812718510627747,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 397.0,
+                    "y": 186.0,
+                    "width": 179.0,
+                    "height": 142.0,
+                    "confidence": 0.6505898833274841,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 587.0,
+                    "y": 207.0,
+                    "width": 215.0,
+                    "height": 249.0,
+                    "confidence": 0.6408542394638062,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 221.0,
+                    "y": 333.0,
+                    "width": 232.0,
+                    "height": 242.0,
+                    "confidence": 0.675274670124054,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 663.0,
+                    "y": 390.0,
+                    "width": 193.0,
+                    "height": 182.0,
+                    "confidence": 0.6492464542388916,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 798.0,
+                    "y": 317.0,
+                    "width": 237.0,
+                    "height": 265.0,
+                    "confidence": 0.6517468690872192,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+                {
+                    "x": 234.0,
+                    "y": 637.0,
+                    "width": 216.0,
+                    "height": 286.0,
+                    "confidence": 0.5249432325363159,
+                    "class_name": "apple",
+                    "class_confidence": None,
+                    "class_id": 0,
+                    "tracker_id": None,
+                },
+            ],
+        },
     }
 ]
 
@@ -45,9 +152,7 @@ def test_grounding_dino(test):
         try:
             assert data == test
         except:
-            print(
-                f"Invalid response: {data}, expected {test['expected_response']}"
-            )
+            print(f"Invalid response: {data}, expected {test['expected_response']}")
     except Exception as e:
         raise e
 
