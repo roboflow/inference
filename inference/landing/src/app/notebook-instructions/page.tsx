ff04ca7e584255fcd85424a9ab4555a349e4cf38
kresetar <korryn@roboflow.com>
2023-12-28 14:48:54 -0500

    s-s-styling!

--- inference/landing/src/app/notebook-instructions/page.tsx
+++ inference/landing/src/app/notebook-instructions/page.tsx
@@ -14,45 +14,60 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-stretch gap-0">
       <div
         id="aboveFold"
-        className="flex flex-col justify-center w-full min-h-screen py-6 md:py-12 overflow-hidden"
+        className="flex flex-col justify-center items-center max-w-full w-full min-h-screen  overflow-hidden"
       >
-        <div className="flex flex-col items-center gap-6 px-8 text-center ">
-          <div className="flex flex-col gap-1 items-center relative z-10">
+        <div className="flex flex-col items-center gap-2 md:gap-10 pb-12 md:px-6 lg:px-10 w-full text-center ">
+          <div className="flex  pt-12 flex-col gap-1 items-center relative z-10">
             <a href="https://roboflow.com" target="_blank">
               <img
                 src="/roboflow_full_logo_color.svg"
                 alt="Roboflow Logo"
                 width={200}
+              
               />
             </a>
-            <div className="font-bold text-gray-900 text-5xl md:text-6xl">
-              Inference Notebook
-            </div>
-            <div
+            
+            <h1 className="font-bold text-gray-900 text-4xl md:text-6xl">
+              Inference
+            </h1>
+            <h2
               className={classNames(
                 roboto_mono.className,
-                "font-bold text-purple-500"
+                "font-bold text-base text-purple-500"
               )}
             >
-              jump into an inference enabled notebook
-            </div>
+                developer-friendly vision inference
+            </h2>
+       
           </div>
+              <div className="flex w-full xl:w-[1000px] max-w-[1000px] bg-white  py-10 bg-opacity-30 border border-white rounded justify-start items-center flex-col px-3 md:px-6 xl:px-12 gap-2 lg:gap-4">
 
-          <div className="flex items-center justify-center gap-2 flex-col sm:flex-row flex-nowrap sm:flex-wrap px-6">
-            To use the built in notebooks in Inference, you need to enable the
-            notebooks feature via the environment variable NOTEBOOK_ENABLED.
-          </div>
-          <div className="flex items-center justify-center gap-2 flex-col sm:flex-row flex-nowrap sm:flex-wrap px-6">
-            To do this, use the `--dev` flag with the inference-cli: `inference
-            server start --dev`. Or, update your docker run command with the
-            argument `-e NOTEBOOK_ENABLED=true`
-          </div>
-          <HeaderLink
+                <h3 className="px-2 lg:px-0 text-xl md:text-3xl text-left  font-semibold 
+                text-gray-900 w-full flex pb-3 ">Jump Into an Inference Enabled Notebook</h3>
+                <div className="flex w-full flex-row gap-3 ">
+                  <span>•</span>
+                  <span className="w-full leading-loose justify-start xl:leading-relaxed text-sm lg:text-base items-baseline gap-0 text-left">
+                  To use the built in notebooks in Inference, you need to enable the
+                  notebooks feature via the environment variable <span className="font-mono text-xs lg:text-sm font-semibold py-1 px-2 border border-gray-400 text-gray-700 rounded">NOTEBOOK_ENABLED</span> .
+                  </span>
+                </div>
+                <div className="flex py-3 flex-row gap-3 ">
+                  <span>•</span>
+                  <span className="w-full leading-loose lg:leading-7 text-sm lg:text-base items-baseline gap-0 text-left">
+                  To do this, use the <span className="font-mono whitespace-break-spaces break-normal  text-xs lg:text-[13px] bg-black bg-opacity-90 font-normal py-1 px-2 border border-gray-900 mx-1 text-white rounded">--dev</span> flag with the inference-cli: <span className="font-mono break-normal whitespace-break-spaces text-xs lg:text-[13px] bg-black bg-opacity-90 font-normal py-1 px-2 border border-gray-900 mx-1 text-white rounded">inference
+            server start --dev</span>. Or, update your docker run command with the
+            argument <span className="font-mono whitespace-break-spaces text-xs break-normal lg:text-[13px] bg-black bg-opacity-90 font-normal py-1 px-2 border border-gray-900 mx-1 text-white rounded">-e NOTEBOOK_ENABLED=true`NOTEBOOK_ENABLED</span>.</span> 
+                 
+                </div>
+          
+          <a
             href="http://localhost:9001/notebook/start"
-            className=""
-            label="Launch Notebook"
-            icon="📓"
-          />
+            className="mt-6 w-max flex flex-row text-white items-center justify-center text-sm lg:text-base rounded py-3 px-8 hover:bg-purple-600 transition duration-400  bg-purple-500 "
+            target="_blank"
+        >Launch Notebook <div className="pl-2 font-bold">→</div>
+          </a>
+              </div>
+          
         </div>
       </div>
       <div id="dividerGradient" className="h-0.5 sm:h-1 w-full">
