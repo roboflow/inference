6894e1f6337d47a7a2c207512dffd12ca29c30d4
Paul <paul@roboflow.com>
2023-12-28 10:25:36 -0700

    Made some fixes based on failing unit tests, and added some new unit tests based on new logic.

--- inference/core/registries/roboflow.py
+++ inference/core/registries/roboflow.py
@@ -5,7 +5,7 @@ from inference.core.cache import cache
 from inference.core.devices.utils import GLOBAL_DEVICE_ID
 from inference.core.entities.types import DatasetID, ModelType, TaskType, VersionID
 from inference.core.env import MODEL_CACHE_DIR
-from inference.core.exceptions import ModelNotRecognisedError
+from inference.core.exceptions import MissingApiKeyError, ModelNotRecognisedError
 from inference.core.logger import logger
 from inference.core.models.base import Model
 from inference.core.registries.base import ModelRegistry
@@ -13,7 +13,9 @@ from inference.core.roboflow_api import (
     MODEL_TYPE_KEY,
     PROJECT_TASK_TYPE_KEY,
     ModelEndpointType,
+    get_roboflow_dataset_type,
     get_roboflow_model_data,
+    get_roboflow_workspace,
 )
 from inference.core.utils.file_system import dump_json, read_json
 from inference.core.utils.roboflow import get_model_id_chunks
@@ -83,7 +85,23 @@ def get_model_type(
     )
     if cached_metadata is not None:
         return cached_metadata[0], cached_metadata[1]
-
+    if version_id == STUB_VERSION_ID:
+        if api_key is None:
+            raise MissingApiKeyError(
+                "Stub model version provided but no API key was provided. API key is required to load stub models."
+            )
+        workspace_id = get_roboflow_workspace(api_key=api_key)
+        project_task_type = get_roboflow_dataset_type(
+            api_key=api_key, workspace_id=workspace_id, dataset_id=dataset_id
+        )
+        model_type = "stub"
+        save_model_metadata_in_cache(
+            dataset_id=dataset_id,
+            version_id=version_id,
+            project_task_type=project_task_type,
+            model_type=model_type,
+        )
+        return project_task_type, model_type
     api_data = get_roboflow_model_data(
         api_key=api_key,
         model_id=model_id,
@@ -92,6 +110,12 @@ def get_model_type(
     ).get("ort")
     project_task_type = api_data.get("type", "object-detection")
     model_type = api_data.get("modelType")
+    save_model_metadata_in_cache(
+        dataset_id=dataset_id,
+        version_id=version_id,
+        project_task_type=project_task_type,
+        model_type=model_type,
+    )
 
     return project_task_type, model_type
 
