604624b0c007e277748311e81c2c23f4b89a257a
Paul Guerrie <paul@roboflow.com>
2024-01-16 20:57:03 +0000

    Make links to notebook on landing page relative instead of hard coded

--- inference/landing/out/_next/static/gTsF1o2W39q4cfp-oFk-l/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b646007f40c4f0a8.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
