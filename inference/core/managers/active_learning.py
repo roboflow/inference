a484c37c62efa534d4f7a721075445c4f5b5e10a
Paweł Pęczek <pawel@roboflow.com>
2024-02-26 17:39:21 +0100

    Make linters happy

--- inference/core/managers/active_learning.py
+++ inference/core/managers/active_learning.py
@@ -39,7 +39,9 @@ class ActiveLearningManager(ModelManager):
         active_learning_disabled_for_request = getattr(
             request, DISABLE_ACTIVE_LEARNING_PARAM, False
         )
-        active_learning_api_key = getattr(request, "active_learning_api_key", None) or getattr(request, "api_key", None)
+        active_learning_api_key = getattr(
+            request, "active_learning_api_key", None
+        ) or getattr(request, "api_key", None)
         if (
             not active_learning_eligible
             or active_learning_disabled_for_request
@@ -150,7 +152,9 @@ class BackgroundTaskActiveLearningManager(ActiveLearningManager):
         prediction = await super().infer_from_request(
             model_id=model_id, request=request, **kwargs
         )
-        active_learning_api_key = getattr(request, "active_learning_api_key", None) or getattr(request, "api_key", None)
+        active_learning_api_key = getattr(
+            request, "active_learning_api_key", None
+        ) or getattr(request, "api_key", None)
         if (
             not active_learning_eligible
             or active_learning_disabled_for_request
