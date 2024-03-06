b86e31c9cdae47ff7298b41eaf70fed8c76d8ebc
Paweł Pęczek <pawel@roboflow.com>
2024-03-06 14:52:43 +0100

    Make linters happy

--- inference/core/interfaces/camera/video_source.py
+++ inference/core/interfaces/camera/video_source.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from enum import Enum
 from queue import Empty, Queue
 from threading import Event, Lock, Thread
-from typing import Any, Callable, List, Optional, Protocol, Union, Dict
+from typing import Any, Callable, Dict, List, Optional, Protocol, Union
 
 import cv2
 import supervision as sv
