8fa51dd9907933f23f6c7d1a953437bc08853226
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-05-14 13:29:42 +0200

    temporarily restore inference.core.workflows.common.utils.detection_to_xyxy

--- inference/core/workflows/core_steps/common/utils.py
+++ inference/core/workflows/core_steps/common/utils.py
@@ -190,3 +190,12 @@ def extract_origin_size_from_images_batch(
                 {HEIGHT_KEY: decoded_image.shape[0], WIDTH_KEY: decoded_image.shape[1]}
             )
     return result
+
+
+# TODO: remove once fusion is migrated
+def detection_to_xyxy(detection: dict) -> Tuple[int, int, int, int]:
+    x_min = round(detection["x"] - detection[WIDTH_KEY] / 2)
+    y_min = round(detection["y"] - detection[HEIGHT_KEY] / 2)
+    x_max = round(x_min + detection[WIDTH_KEY])
+    y_max = round(y_min + detection[HEIGHT_KEY])
+    return x_min, y_min, x_max, y_max
