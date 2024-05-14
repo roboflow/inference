19c90eb3cd2d735fdb52885d25d211e124b6f075
Sam Beran <sberan@gmail.com>
2024-05-14 12:53:08 -0500

    document source_id parameter of VideoFrame

--- inference/core/interfaces/camera/entities.py
+++ inference/core/interfaces/camera/entities.py
@@ -53,6 +53,7 @@ class VideoFrame:
         image (np.ndarray): The image data of the frame as a NumPy array.
         frame_id (FrameID): A unique identifier for the frame.
         frame_timestamp (FrameTimestamp): The timestamp when the frame was captured.
+        source_id (int): The index of the video_reference element which was passed to InferencePipeline for this frame (useful when multiple streams are passed to InferencePipeline).
     """
 
     image: np.ndarray
