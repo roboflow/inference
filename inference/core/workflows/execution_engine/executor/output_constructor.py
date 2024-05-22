8c645c6a2eb00abbf312b57d130205f880a467cb
Paweł Pęczek <pawel@roboflow.com>
2024-05-22 09:55:22 +0200

    Apply changes to fix logic and add tests

--- inference/core/workflows/execution_engine/executor/output_constructor.py
+++ inference/core/workflows/execution_engine/executor/output_constructor.py
@@ -5,6 +5,7 @@ from inference.core.workflows.entities.base import CoordinatesSystem, JsonField
 from inference.core.workflows.execution_engine.compiler.utils import (
     get_last_chunk_of_selector,
     get_step_selector_from_its_output,
+    is_input_selector,
 )
 from inference.core.workflows.execution_engine.executor.execution_cache import (
     ExecutionCache,
@@ -18,16 +19,19 @@ def construct_workflow_output(
 ) -> Dict[str, List[Any]]:
     result = {}
     for node in workflow_outputs:
+        if is_input_selector(selector_or_value=node.selector):
+            input_name = get_last_chunk_of_selector(selector=node.selector)
+            result[node.name] = runtime_parameters[input_name]
+            # above returns List[<image>]
+            # for image input and value of parameter for singular input, we do not
+            # check parameter existence, as that should be checked by EE at compilation
+            continue
         step_selector = get_step_selector_from_its_output(
             step_output_selector=node.selector
         )
         step_name = get_last_chunk_of_selector(selector=step_selector)
         cache_contains_step = execution_cache.contains_step(step_name=step_name)
         if not cache_contains_step:
-            if runtime_parameters.get(step_name) is not None:
-                result[step_name] = [runtime_parameters[step_name]]
-            else:
-                result[node.name] = []
             continue
         if node.selector.endswith(".*"):
             result[node.name] = construct_wildcard_output(
