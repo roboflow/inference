d9ca600af4067c10937525b0a3dd71f41137a623
Nick Herrig <neherrig@gmail.com>
2024-05-24 18:42:27 -0500

    Run make style

--- inference/models/yolov10/yolov10_object_detection.py
+++ inference/models/yolov10/yolov10_object_detection.py
@@ -19,7 +19,9 @@ class YOLOv10ObjectDetection(ObjectDetectionBaseOnnxRoboflowInferenceModel):
     Methods:
         predict: Performs object detection on the given image using the ONNX session.
     """
+
     box_format = "xyxy"
+
     @property
     def weights_file(self) -> str:
         """Gets the weights file for the YOLOv10 model.
@@ -39,10 +41,12 @@ class YOLOv10ObjectDetection(ObjectDetectionBaseOnnxRoboflowInferenceModel):
             Tuple[np.ndarray]: NumPy array representing the predictions, including boxes, confidence scores, and class confidence scores.
         """
         predictions = self.onnx_session.run(None, {self.input_name: img_in})[0]
-        boxes = predictions[:, :, :4] 
+        boxes = predictions[:, :, :4]
         batch_size = boxes.shape[0]
         num_boxes = boxes.shape[1]
-        class_confs = np.zeros((batch_size, num_boxes, self.num_classes), dtype=np.float32)
+        class_confs = np.zeros(
+            (batch_size, num_boxes, self.num_classes), dtype=np.float32
+        )
         for batch in range(batch_size):
             for box in range(num_boxes):
                 class_id = int(predictions[batch, box, 5])
@@ -51,5 +55,5 @@ class YOLOv10ObjectDetection(ObjectDetectionBaseOnnxRoboflowInferenceModel):
 
         confs = predictions[:, :, 4:5]
         predictions = np.concatenate([boxes, confs, class_confs], axis=2)
-    
+
         return (predictions,)
