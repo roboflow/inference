c23135a58a00c2fea5ca24a5baa71c27b4b60a33
Brad Dwyer <brad@roboflow.com>
2024-06-03 20:27:12 +0200

    Change default values & description

--- inference/core/workflows/core_steps/models/foundation/yolo_world.py
+++ inference/core/workflows/core_steps/models/foundation/yolo_world.py
@@ -71,8 +71,8 @@ class BlockManifest(WorkflowBlockManifest):
     class_names: Union[
         WorkflowParameterSelector(kind=[LIST_OF_VALUES_KIND]), List[str]
     ] = Field(
-        description="List of classes to use YoloWorld model against",
-        examples=[["a", "b", "c"], "$inputs.class_names"],
+        description="One or more classes that you want YOLO-World to detect. The model accepts any string as an input, though does best with short descriptions of common objects.",
+        examples=[["person", "car", "license plate"], "$inputs.class_names"],
     )
     version: Union[
         Literal["s", "m", "l", "x", "v2-s", "v2-m", "v2-l", "v2-x"],
@@ -86,7 +86,7 @@ class BlockManifest(WorkflowBlockManifest):
         Optional[FloatZeroToOne],
         WorkflowParameterSelector(kind=[FLOAT_ZERO_TO_ONE_KIND]),
     ] = Field(
-        default=0.4,
+        default=0.005,
         description="Confidence threshold for detections",
         examples=[0.3, "$inputs.confidence"],
     )
