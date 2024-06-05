4220c721ec271119710f3357678cba20ff512e60
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-06-05 09:36:48 +0200

    Split tests, add description to individual asserts

--- tests/workflows/unit_tests/core_steps/transformations/test_perspective_correction.py
+++ tests/workflows/unit_tests/core_steps/transformations/test_perspective_correction.py
@@ -12,206 +12,160 @@ from inference.core.workflows.core_steps.transformations.perspective_correction 
     extend_perspective_polygon,
     generate_transformation_matrix,
 )
+from inference.core.workflows.entities.base import Batch
+
+
+@pytest.mark.parametrize("broken_input", [1, "cat", np.array([])])
+def test_pick_largest_perspective_polygons_raises_on_unexpected_type_of_input(broken_input):
+    with pytest.raises(ValueError, match="Unexpected type of input"):
+        pick_largest_perspective_polygons(perspective_polygons_batch=broken_input)
+
+
+@pytest.mark.parametrize("broken_input", [[1], ["cat"]])
+def test_pick_largest_perspective_polygons_raises_on_unexpected_type_of_batch_element(broken_input):
+    with pytest.raises(ValueError, match="Unexpected type of batch element"):
+        pick_largest_perspective_polygons(perspective_polygons_batch=broken_input)
+
+
+@pytest.mark.parametrize("empty_batch", [[], Batch([])])
+def test_pick_largest_perspective_polygons_raises_on_empty_batch(empty_batch):
+    with pytest.raises(ValueError, match="Unexpected empty batch"):
+        pick_largest_perspective_polygons(perspective_polygons_batch=empty_batch)
 
 
 PERSPECTIVE_POLYGON_LIST = [[1, 1], [10, 1], [10, 10], [1, 10]]
-LARGE_PERSPECTIVE_POLYGON_LIST = [[1, 1], [100, 1], [100, 100], [1, 100]]
 PERSPECTIVE_POLYGON_NP_ARRAY = np.array(PERSPECTIVE_POLYGON_LIST)
-LARGE_PERSPECTIVE_POLYGON_NP_ARRAY = np.array(LARGE_PERSPECTIVE_POLYGON_LIST)
 
 
 @pytest.mark.parametrize(
-    "perspective_polygons_batch, expected_exception",
+    "empty_batch_elements",
     [
-        (1, pytest.raises(ValueError, match="Unexpected type of input")),
-        ("cat", pytest.raises(ValueError, match="Unexpected type of input")),
-        (np.array([]), pytest.raises(ValueError, match="Unexpected type of input")),
-        (np.array([1, 2, 3, 4]), pytest.raises(ValueError, match="Unexpected type of input")),
-        (PERSPECTIVE_POLYGON_NP_ARRAY, pytest.raises(ValueError, match="Unexpected type of input")),
-        ([], pytest.raises(ValueError, match="Unexpected empty batch")),
-        ([[], []], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([np.array([]), np.array([])], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([np.array([[1, 2], [3, 4], [5, 6]])], pytest.raises(ValueError, match="Unexpected shape of batch element")),
-        ([[PERSPECTIVE_POLYGON_NP_ARRAY], []], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([PERSPECTIVE_POLYGON_NP_ARRAY, np.array([])], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([[PERSPECTIVE_POLYGON_LIST], []], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([PERSPECTIVE_POLYGON_LIST, []], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([1, 2, 3, 4], pytest.raises(ValueError, match="Unexpected type of batch element")),
-        (PERSPECTIVE_POLYGON_LIST, pytest.raises(ValueError, match="No batch element consists of 4 vertices")),
-        ([[1, 2], [3, 4], [5, 6]], pytest.raises(ValueError, match="No batch element consists of 4 vertices")),
-        ([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]], pytest.raises(ValueError, match="No batch element consists of 4 vertices")),
-        ([[], []], pytest.raises(ValueError, match="Unexpected empty batch element")),
-        ([PERSPECTIVE_POLYGON_NP_ARRAY], does_not_raise()),
-        ([[PERSPECTIVE_POLYGON_NP_ARRAY]], does_not_raise()),
-        ([[PERSPECTIVE_POLYGON_NP_ARRAY, []]], does_not_raise()),
-        ([PERSPECTIVE_POLYGON_LIST], does_not_raise()),
-        ([[PERSPECTIVE_POLYGON_LIST]], does_not_raise()),
-        ([[PERSPECTIVE_POLYGON_LIST, []]], does_not_raise()),
-    ]
+        [[]],
+        [np.array([])],
+        [[PERSPECTIVE_POLYGON_NP_ARRAY], []],
+        [[PERSPECTIVE_POLYGON_LIST], []]
+    ],
 )
-def test_pick_largest_perspective_polygons_exceptions(perspective_polygons_batch: Any, expected_exception):
-    with expected_exception:
-        pick_largest_perspective_polygons(perspective_polygons_batch=perspective_polygons_batch)
+def test_pick_largest_perspective_polygons_raises_on_empty_batch_element(empty_batch_elements):
+    with pytest.raises(ValueError, match="Unexpected empty batch element"):
+        pick_largest_perspective_polygons(perspective_polygons_batch=empty_batch_elements)
 
 
-@pytest.mark.parametrize(
-    "perspective_polygons_batch, expected_largest_polygons",
-    [
-        ([PERSPECTIVE_POLYGON_LIST], [PERSPECTIVE_POLYGON_NP_ARRAY]),
-        ([PERSPECTIVE_POLYGON_LIST, PERSPECTIVE_POLYGON_LIST], [PERSPECTIVE_POLYGON_NP_ARRAY, PERSPECTIVE_POLYGON_NP_ARRAY]),
-        ([PERSPECTIVE_POLYGON_LIST, LARGE_PERSPECTIVE_POLYGON_LIST], [PERSPECTIVE_POLYGON_NP_ARRAY, LARGE_PERSPECTIVE_POLYGON_NP_ARRAY]),
-        ([[PERSPECTIVE_POLYGON_LIST, PERSPECTIVE_POLYGON_LIST]], [PERSPECTIVE_POLYGON_NP_ARRAY]),
-        ([[PERSPECTIVE_POLYGON_LIST, LARGE_PERSPECTIVE_POLYGON_LIST]], [LARGE_PERSPECTIVE_POLYGON_NP_ARRAY]),
-        (
-            [
-                [PERSPECTIVE_POLYGON_LIST, LARGE_PERSPECTIVE_POLYGON_LIST],
-                [PERSPECTIVE_POLYGON_LIST, LARGE_PERSPECTIVE_POLYGON_LIST]
-            ],
-            [
-                LARGE_PERSPECTIVE_POLYGON_NP_ARRAY,
-                LARGE_PERSPECTIVE_POLYGON_NP_ARRAY
-            ]
-        ),
+def test_pick_largest_perspective_polygons():
+    # given
+    small_perspective_pollygon = [[1, 1], [10, 1], [10, 10], [1, 10]]
+    large_perspective_pollygon = [[1, 1], [100, 1], [100, 100], [1, 100]]
+    batch = [
+        [small_perspective_pollygon, large_perspective_pollygon],
+        [small_perspective_pollygon, large_perspective_pollygon]
     ]
-)
-def test_pick_largest_perspective_polygons(perspective_polygons_batch, expected_largest_polygons):
-    largest_polygons = pick_largest_perspective_polygons(perspective_polygons_batch=perspective_polygons_batch)
-    assert isinstance(largest_polygons, list)
-    assert len(largest_polygons) == len(expected_largest_polygons)
-    assert all(np.array_equal(l, e) for l, e in zip(largest_polygons, expected_largest_polygons))
+
+    # when
+    largest_polygons = pick_largest_perspective_polygons(perspective_polygons_batch=batch)
+
+    # then
+    assert len(largest_polygons) == 2, "Output batch size must be the same as input batch size"
+    assert np.array_equal(largest_polygons[0], large_perspective_pollygon), "Largest polygon must be picked"
+    assert np.array_equal(largest_polygons[1], large_perspective_pollygon), "Largest polygon must be picked"
 
 
-COUNTER_CLOCKWISE_POLYGON = np.array([[1, 1], [1, 10], [10, 10], [10, 1]])
-CLOCKWISE_POLYGON = np.array([[1, 10], [1, 1], [10, 1], [10, 10]])
+def test_sort_polygon_vertices_clockwise():
+    # when
+    clockwise_polygon = sort_polygon_vertices_clockwise(polygon=np.array([[1, 1], [1, 10], [10, 10], [10, 1]]))
+
+    # then
+    assert np.array_equal(clockwise_polygon, np.array([[1, 10], [1, 1], [10, 1], [10, 10]])), "Polygon vertices must be sorted clockwise"
 
 
-@pytest.mark.parametrize(
-    "polygon, expected_clockwise_polygon",
-    [
-        (COUNTER_CLOCKWISE_POLYGON, CLOCKWISE_POLYGON),
-        (np.roll(COUNTER_CLOCKWISE_POLYGON, 2), CLOCKWISE_POLYGON),
-        (np.roll(COUNTER_CLOCKWISE_POLYGON, 4), CLOCKWISE_POLYGON),
-        (np.roll(COUNTER_CLOCKWISE_POLYGON, 6), CLOCKWISE_POLYGON),
-    ]
-)
-def test_sort_polygon_vertices_clockwise(polygon: np.ndarray, expected_clockwise_polygon: np.array):
-    clockwise_polygon = sort_polygon_vertices_clockwise(polygon=polygon)
-    assert np.array_equal(clockwise_polygon, expected_clockwise_polygon)
+def test_roll_polygon_vertices_to_start_from_leftmost_bottom():
+    # when
+    rolled_polygon = roll_polygon_vertices_to_start_from_leftmost_bottom(
+        polygon=np.array([[1, 1], [10, 1], [10, 10], [1, 10]])
+    )
+
+    # then
+    assert np.array_equal(
+        rolled_polygon,
+        np.array([[1, 10], [1, 1], [10, 1], [10, 10]])
+    ), "Polygon must be reorganized so leftmost bottom "
 
 
-POLYGON = np.array([[1, 1], [10, 1], [10, 10], [1, 10]])
-ROLLED_POLYGON = np.array([[1, 10], [1, 1], [10, 1], [10, 10]])
+def test_extend_perspective_polygon_detections_within_polygon():
+    # given
+    polygon = np.array([[100, 110], [100, 100], [110, 100], [110, 110]])
+    detections_within_polygon = sv.Detections(xyxy=np.array([[105, 105, 106, 106]]))
+    detections_on_the_corners = sv.Detections(xyxy=np.array([
+        [99, 99, 101, 100],
+        [109, 99, 111, 100],
+        [99, 109, 101, 110],
+        [109, 109, 111, 110]
+    ]))
+    detections = sv.Detections.merge([
+        detections_within_polygon,
+        detections_on_the_corners,
+    ])
 
-
-@pytest.mark.parametrize(
-    "polygon, expected_rolled_polygon",
-    [
-        (POLYGON, ROLLED_POLYGON),
-        (np.roll(POLYGON, 2), ROLLED_POLYGON),
-        (np.roll(POLYGON, 4), ROLLED_POLYGON),
-        (np.roll(POLYGON, 6), ROLLED_POLYGON),
-    ]
-)
-def test_roll_polygon_vertices_to_start_from_leftmost_bottom(polygon: np.ndarray, expected_rolled_polygon: np.array):
-    rolled_polygon = roll_polygon_vertices_to_start_from_leftmost_bottom(polygon=polygon)
-    assert np.array_equal(rolled_polygon, expected_rolled_polygon)
-
-
-ORIG_POLYGON = np.array([[100, 110], [100, 100], [110, 100], [110, 110]])
-DETECTIONS_WITHIN_POLYGON = sv.Detections(xyxy=np.array([[105, 105, 106, 106]]))
-DETECTIONS_ON_THE_CORNERS = sv.Detections(xyxy=np.array([[99, 99, 101, 100], [109, 99, 111, 100], [99, 109, 101, 110], [109, 109, 111, 110]]))
-DETECTIONS_EXTENDING_FROM_THE_LEFT = sv.Detections(xyxy=np.array([[90, 105, 92, 106]]))
-DETECTIONS_EXTENDING_FROM_THE_RIGHT = sv.Detections(xyxy=np.array([[120, 105, 122, 106]]))
-DETECTIONS_EXTENDING_FROM_THE_TOP = sv.Detections(xyxy=np.array([[105, 90, 106, 92]]))
-DETECTIONS_EXTENDING_FROM_THE_BOTTOM = sv.Detections(xyxy=np.array([[105, 120, 106, 122]]))
-
-
-@pytest.mark.parametrize(
-    "polygon, detections, bbox_position, expected_extended_polygon",
-    [
-        (ORIG_POLYGON, sv.Detections.empty(), "", ORIG_POLYGON),
-        (ORIG_POLYGON, sv.Detections.empty(), sv.Position.BOTTOM_CENTER, ORIG_POLYGON),
-        (ORIG_POLYGON, DETECTIONS_WITHIN_POLYGON, sv.Position.BOTTOM_CENTER, ORIG_POLYGON),
-        (ORIG_POLYGON, DETECTIONS_ON_THE_CORNERS, sv.Position.BOTTOM_CENTER, ORIG_POLYGON),
-        (
-            ORIG_POLYGON,
-            sv.Detections.merge([DETECTIONS_WITHIN_POLYGON, DETECTIONS_EXTENDING_FROM_THE_LEFT]),
-            sv.Position.BOTTOM_CENTER,
-            np.array([[91, 110], [91, 100], [110, 100], [110, 110]])
-        ),
-        (
-            ORIG_POLYGON,
-            sv.Detections.merge([DETECTIONS_WITHIN_POLYGON, DETECTIONS_EXTENDING_FROM_THE_RIGHT]),
-            sv.Position.BOTTOM_CENTER,
-            np.array([[100, 110], [100, 100], [121, 100], [121, 110]])
-        ),
-        (
-            ORIG_POLYGON,
-            sv.Detections.merge([DETECTIONS_WITHIN_POLYGON, DETECTIONS_EXTENDING_FROM_THE_TOP]),
-            sv.Position.BOTTOM_CENTER,
-            np.array([[100, 110], [100, 92], [110, 92], [110, 110]])
-        ),
-        (
-            ORIG_POLYGON,
-            sv.Detections.merge([DETECTIONS_WITHIN_POLYGON, DETECTIONS_EXTENDING_FROM_THE_BOTTOM]),
-            sv.Position.BOTTOM_CENTER,
-            np.array([[100, 122], [100, 100], [110, 100], [110, 122]])
-        ),
-        (
-            ORIG_POLYGON,
-            sv.Detections.merge([
-                DETECTIONS_WITHIN_POLYGON,
-                DETECTIONS_EXTENDING_FROM_THE_LEFT,
-                DETECTIONS_EXTENDING_FROM_THE_RIGHT,
-                DETECTIONS_EXTENDING_FROM_THE_TOP,
-                DETECTIONS_EXTENDING_FROM_THE_BOTTOM
-            ]),
-            sv.Position.BOTTOM_CENTER,
-            np.array([[91, 122], [91, 92], [121, 92], [121, 122]])
-        ),
-    ]
-)
-def test_extend_perspective_polygon(polygon: np.ndarray, detections: sv.Detections, bbox_position: sv.Position, expected_extended_polygon: np.array):
+    # when
     extended_polygon = extend_perspective_polygon(
         polygon=polygon,
         detections=detections,
-        bbox_position=bbox_position,
+        bbox_position=sv.Position.BOTTOM_CENTER,
     )
-    assert np.array_equal(extended_polygon, expected_extended_polygon)
+
+    # then
+    assert np.array_equal(
+        extended_polygon,
+        np.array([[100, 110], [100, 100], [110, 100], [110, 110]])
+    ), "Detections within polygon must not modify it"
 
 
-@pytest.mark.parametrize(
-    "src_polygon, detections, bbox_position, transformed_rect_width, transformed_rect_height, expected_transformation_matrix",
-    [
-        (
-            ORIG_POLYGON,
-            sv.Detections.merge([
-                DETECTIONS_WITHIN_POLYGON,
-                DETECTIONS_ON_THE_CORNERS,
-                DETECTIONS_EXTENDING_FROM_THE_LEFT,
-                DETECTIONS_EXTENDING_FROM_THE_RIGHT,
-                DETECTIONS_EXTENDING_FROM_THE_TOP,
-                DETECTIONS_EXTENDING_FROM_THE_BOTTOM
-            ]),
-            sv.Position.BOTTOM_CENTER,
-            1000,
-            1000,
-            np.array([[       33.3, -4.7325e-16,     -3030.3],
-                      [          0,        33.3,     -3063.6],
-                      [ 7.5867e-18,  1.8967e-18,           1]])
-        ),
-    ]
-)
-def test_generate_transformation_matrix(src_polygon, detections, bbox_position, transformed_rect_width, transformed_rect_height, expected_transformation_matrix):
-    transformation_matrix = generate_transformation_matrix(
-        src_polygon=src_polygon,
+def test_extend_perspective_polygon_detections_within_polygon():
+    # given
+    polygon = np.array([[100, 110], [100, 100], [110, 100], [110, 110]])
+    detections_extending_from_the_left = sv.Detections(xyxy=np.array([[90, 105, 92, 106]]))
+    detections_extending_from_the_right = sv.Detections(xyxy=np.array([[120, 105, 122, 106]]))
+    detections_extending_from_the_top = sv.Detections(xyxy=np.array([[105, 90, 106, 92]]))
+    detections_extending_from_the_bottom = sv.Detections(xyxy=np.array([[105, 120, 106, 122]]))
+    detections = sv.Detections.merge([
+        detections_extending_from_the_left,
+        detections_extending_from_the_right,
+        detections_extending_from_the_top,
+        detections_extending_from_the_bottom,
+    ])
+
+    # when
+    extended_polygon = extend_perspective_polygon(
+        polygon=polygon,
         detections=detections,
-        transformed_rect_width=transformed_rect_width,
-        transformed_rect_height=transformed_rect_height,
-        detections_anchor=bbox_position,
+        bbox_position=sv.Position.BOTTOM_CENTER,
     )
 
-    assert np.allclose(transformation_matrix, expected_transformation_matrix)
+    # then
+    assert np.array_equal(
+        extended_polygon,
+        np.array([[91, 122], [91, 92], [121, 92], [121, 122]])
+    ), "Detections outside of polygon should extend it"
 
 
-    
+def test_generate_transformation_matrix():
+    # given
+    polygon = np.array([[100, 110], [100, 100], [110, 100], [110, 110]])
+
+    # when
+    transformation_matrix = generate_transformation_matrix(
+        src_polygon=polygon,
+        detections=sv.Detections.empty(),
+        transformed_rect_width=1000,
+        transformed_rect_height=1000,
+        detections_anchor=sv.Position.BOTTOM_CENTER,
+    )
+
+    expected_transformation_matrix = np.array([
+        [       99.9,  -5.955e-15,       -9990],
+        [-1.6536e-14,        99.9,       -9990],
+        [         -0,          -0,           1],
+    ])
+    assert np.allclose(
+        transformation_matrix,
+        expected_transformation_matrix
+    ), "Transformation matrix must match"
--- tests/workflows/unit_tests/core_steps/transformations/test_polygon_simplification.py
+++ tests/workflows/unit_tests/core_steps/transformations/test_polygon_simplification.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pytest
 import supervision as sv
 
 from inference.core.workflows.core_steps.transformations.polygon_simplification import (
@@ -7,58 +6,68 @@ from inference.core.workflows.core_steps.transformations.polygon_simplification 
 )
 
 
-@pytest.mark.parametrize(
-    "polygon, required_number_of_vertices, expected_simplified_polygon",
-    [
-        (
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]]),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-        (
-            np.roll(np.array([[10, 1], [10, 10], [20, 10], [20, 1]]), shift=2),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-        (
-            np.array([[10, 10], [10, 1], [20, 1], [20, 10]]),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-        (
-            np.roll(np.array([[10, 10], [10, 1], [20, 1], [20, 10]]), shift=2),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-        (
-            np.array([[10, 10], [10, 5], [10, 1], [15, 1], [20, 1], [20, 5], [20, 10]]),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-        (
-            np.array([[10, 10], [5, 5], [10, 1], [15, 1], [20, 1], [25, 5], [20, 10]]),
-            4,
-            np.array([[5, 5], [10, 10], [25, 5], [20, 1]])
-        ),
-        (  # drop valley
-            np.roll(np.array([[10, 10], [10, 1], [15, 1], [15, 9], [16, 1], [20, 1], [20, 10]]), shift=2),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-        (  # drop two valleys
-            np.roll(np.array([[10, 10], [10, 1], [15, 1], [15, 9], [16, 1], [20, 1], [20, 10], [18, 10], [17, 2], [16, 10]]), shift=2),
-            4,
-            np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
-        ),
-    ]
-)
-def test_calculate_simplified_polygon(polygon, required_number_of_vertices, expected_simplified_polygon):
+def test_calculate_simplified_polygon_no_simplification_required():
+    # given
+    polygon = np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
     mask = sv.polygon_to_mask(
         polygon=polygon,
         resolution_wh=(np.max(polygon, axis=0) + 10)
     )
+
+    # when
     simplified_polygon = calculate_simplified_polygon(
         mask=mask,
-        required_number_of_vertices=required_number_of_vertices,
+        required_number_of_vertices=len(polygon),
     )
-    assert np.allclose(simplified_polygon, expected_simplified_polygon)
+
+    # then
+    assert np.allclose(
+        simplified_polygon,
+        np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
+    ), "Polygon should not be modified if it already contains required number of vertices"
+
+
+def test_calculate_simplified_polygon_resulting_in_convex_polygon():
+    # given
+    polygon = np.array([[10, 10], [10, 1], [15, 1], [15, 9], [16, 1], [20, 1], [20, 10], [18, 10], [17, 2], [16, 10]])
+    mask = sv.polygon_to_mask(
+        polygon=polygon,
+        resolution_wh=(np.max(polygon, axis=0) + 10)
+    )
+
+    # when
+    simplified_polygon = calculate_simplified_polygon(
+        mask=mask,
+        required_number_of_vertices=4,
+    )
+
+    # then
+    assert np.allclose(
+        simplified_polygon,
+        np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
+    ), "Valleys ([15, 1], [15, 9], [16, 1]) on the edge between [10, 1] and [20, 1] " \
+        "and ([18, 10], [17, 2], [16, 10]) on the edge between [20, 10] and [10, 10] " \
+        "should be dropped and shape of the polygon should remain unchanged"
+
+
+def test_calculate_simplified_polygon_drop_intermediate_points():
+    # given
+    polygon = np.array(np.array([[10, 10], [10, 5], [10, 1], [15, 1], [20, 1], [20, 5], [20, 10]]))
+    mask = sv.polygon_to_mask(
+        polygon=polygon,
+        resolution_wh=(np.max(polygon, axis=0) + 10)
+    )
+
+    # when
+    simplified_polygon = calculate_simplified_polygon(
+        mask=mask,
+        required_number_of_vertices=4,
+    )
+
+    # then
+    assert np.allclose(
+        simplified_polygon,
+        np.array([[10, 1], [10, 10], [20, 10], [20, 1]])
+    ), "Intermediate points [10, 5] (between [10, 10] and [10, 1]), " \
+        "[15, 1] (between [10, 1] and [20, 1]) and [20, 5] (between [20, 1] and [20, 10]) " \
+        "should be dropped and shape of polygon should remain unchanged."
