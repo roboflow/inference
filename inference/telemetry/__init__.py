6c25b09c50a102b0e78bb45e5d7f84018437c85d
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-06-17 10:36:03 +0200

    WIP

--- inference/telemetry/__init__.py
+++ inference/telemetry/__init__.py
@@ -1,1 +1,11 @@
+"""
+In order to enable Roboflow to perform our business functions
+we need to be able to measure how our deployments are used.
+
+Users who don't use API key and who don't use premium or enterprise workflow blocks
+can turn the telemetry off by setting TELEMETRY_OPT_OUT environment variable to True.
+
+Please consult the license for more information regarding when telemetry cannot be turned off.
+"""
+
 from .collector import collector
