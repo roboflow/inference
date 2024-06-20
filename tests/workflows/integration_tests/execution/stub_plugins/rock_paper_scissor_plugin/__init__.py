6a99cf61be2eb3ffd7ceffccd02cddb8fd70ccc6
Paweł Pęczek <pawel@roboflow.com>
2024-06-20 10:42:49 +0200

    Add dedicated moudle for stub plugins

--- tests/workflows/integration_tests/execution/stub_plugins/rock_paper_scissor_plugin/__init__.py
+++ tests/workflows/integration_tests/execution/stub_plugins/rock_paper_scissor_plugin/__init__.py
@@ -1,10 +1,10 @@
 from typing import List, Type
 
 from inference.core.workflows.prototypes.block import WorkflowBlock
-from tests.workflows.integration_tests.execution.rock_paper_scissor_plugin.expression import (
+from tests.workflows.integration_tests.execution.stub_plugins.rock_paper_scissor_plugin.expression import (
     ExpressionBlock,
 )
-from tests.workflows.integration_tests.execution.rock_paper_scissor_plugin.take_first_non_empty import (
+from tests.workflows.integration_tests.execution.stub_plugins.rock_paper_scissor_plugin.take_first_non_empty import (
     TakeFirstNonEmptyBlock,
 )
 
