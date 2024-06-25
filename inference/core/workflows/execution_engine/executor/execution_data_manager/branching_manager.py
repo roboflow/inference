37f16567575b7c8f405e9c302bd0b3ccafbe3275
Paweł Pęczek <pawel@roboflow.com>
2024-06-25 12:59:28 +0200

    Add unit tests for branching manager

--- inference/core/workflows/execution_engine/executor/execution_data_manager/branching_manager.py
+++ inference/core/workflows/execution_engine/executor/execution_data_manager/branching_manager.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Set, Union
 
+from inference.core.workflows.errors import ExecutionEngineRuntimeError
 from inference.core.workflows.execution_engine.executor.execution_data_manager.dynamic_batches_manager import (
     DynamicBatchIndex,
 )
@@ -24,31 +25,47 @@ class BranchingManager:
         mask: Set[DynamicBatchIndex],
     ) -> None:
         if execution_branch in self._masks:
-            raise ValueError(
-                f"Attempted to re-register maks for execution branch: {execution_branch}"
+            raise ExecutionEngineRuntimeError(
+                public_message=f"Attempted to re-register maks for execution branch: {execution_branch}. "
+                f"This is most likely a bug. Contact Roboflow team through github issues "
+                f"(https://github.com/roboflow/inference/issues) providing full context of"
+                f"the problem - including workflow definition you use.",
+                context="workflow_execution | step_input_assembling",
             )
         self._batch_compatibility[execution_branch] = True
         self._masks[execution_branch] = mask
 
     def register_non_batch_mask(self, execution_branch: str, mask: bool) -> None:
         if execution_branch in self._masks:
-            raise ValueError(
-                f"Attempted to re-register maks for execution branch: {execution_branch}"
+            raise ExecutionEngineRuntimeError(
+                public_message=f"Attempted to re-register maks for execution branch: {execution_branch}. "
+                f"This is most likely a bug. Contact Roboflow team through github issues "
+                f"(https://github.com/roboflow/inference/issues) providing full context of"
+                f"the problem - including workflow definition you use.",
+                context="workflow_execution | step_input_assembling",
             )
         self._batch_compatibility[execution_branch] = False
         self._masks[execution_branch] = mask
 
     def get_mask(self, execution_branch: str) -> Union[Set[DynamicBatchIndex], bool]:
         if execution_branch not in self._masks:
-            raise ValueError(
-                f"Attempted to get mask for not registered execution branch: {execution_branch}"
+            raise ExecutionEngineRuntimeError(
+                public_message=f"Attempted to get mask for not registered execution branch: {execution_branch}. "
+                f"This is most likely a bug. Contact Roboflow team through github issues "
+                f"(https://github.com/roboflow/inference/issues) providing full context of"
+                f"the problem - including workflow definition you use.",
+                context="workflow_execution | step_input_assembling",
             )
         return self._masks[execution_branch]
 
     def is_execution_branch_batch_oriented(self, execution_branch: str) -> bool:
         if execution_branch not in self._batch_compatibility:
-            raise ValueError(
-                f"Attempted to get information about not registered execution branch: {execution_branch}"
+            raise ExecutionEngineRuntimeError(
+                public_message=f"Attempted to get info about not registered execution branch: {execution_branch}. "
+                f"This is most likely a bug. Contact Roboflow team through github issues "
+                f"(https://github.com/roboflow/inference/issues) providing full context of"
+                f"the problem - including workflow definition you use.",
+                context="workflow_execution | step_input_assembling",
             )
         return self._batch_compatibility[execution_branch]
 
