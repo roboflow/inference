33e7689417544a48c0c631a7f78148cdaf287160
Peter Robicheaux <peter@roboflow.com>
2024-06-28 21:58:07 +0000

    Ensure non-lora'd works

--- inference/models/florence2/florence2.py
+++ inference/models/florence2/florence2.py
@@ -5,11 +5,38 @@ from transformers import AutoModelForCausalLM
 
 from inference.models.florence2.utils import import_class_from_file
 from inference.models.transformers import LoRATransformerModel, TransformerModel
+from inference.core.env import DEVICE
 
+if DEVICE is None:
+    DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 class Florence2(TransformerModel):
     transformers_class = AutoModelForCausalLM
     default_dtype = torch.float32
+    def initialize_model(self):
+        self.transformers_class = import_class_from_file(
+            os.path.join(self.cache_dir, "modeling_florence2.py"),
+            "Florence2ForConditionalGeneration",
+        )
+
+        self.processor_class = import_class_from_file(
+            os.path.join(self.cache_dir, "processing_florence2.py"),
+            "Florence2Processor",
+        )
+
+        self.model = (
+            self.transformers_class.from_pretrained(
+                self.cache_dir,
+                device_map=DEVICE,
+                token=self.huggingface_token,
+            )
+            .eval()
+            .to(self.dtype)
+        )
+
+        self.processor = AutoProcessor.from_pretrained(
+            self.cache_dir, token=self.huggingface_token
+        )
 
 
 class LoRAFlorence2(LoRATransformerModel):
