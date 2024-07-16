da2049956f789ee4506e2da297a237dd23dad443
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-07-16 10:34:34 +0200

    adjust API endpoint

--- inference/usage_tracking/config.py
+++ inference/usage_tracking/config.py
@@ -14,7 +14,7 @@ from inference.core.utils.url_utils import wrap_url
 class TelemetrySettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="telemetry_")
 
-    api_usage_endpoint_url: str = "https://localhost/usage/inference"
+    api_usage_endpoint_url: str = "https://api.roboflow.one/usage/inference"
     flush_interval: int = Field(default=10, ge=10, le=300)
     opt_out: Optional[bool] = False
     queue_size: int = Field(default=10, ge=10, le=10000)
@@ -23,11 +23,11 @@ class TelemetrySettings(BaseSettings):
     def check_values(cls, inst: TelemetrySettings):
         if PROJECT == "roboflow-platform":
             inst.api_usage_endpoint_url = wrap_url(
-                "https://api.roboflow.com/usage/pingback"
+                "https://api.roboflow.com/usage/inference"
             )
         else:
             inst.api_usage_endpoint_url = wrap_url(
-                "https://api.roboflow.one/usage/pingback"
+                "https://api.roboflow.one/usage/inference"
             )
         inst.flush_interval = min(max(inst.flush_interval, 10), 300)
         inst.queue_size = min(max(inst.queue_size, 10), 10000)
