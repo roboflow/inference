c4a5eaff7e9221ef62ed10c83aea03382c6aafea
Brad Dwyer <brad@roboflow.com>
2024-07-17 15:32:58 -0700

    Add test of copy=False

--- tests/workflows/unit_tests/core_steps/visualizations/test_bounding_box.py
+++ tests/workflows/unit_tests/core_steps/visualizations/test_bounding_box.py
@@ -62,10 +62,11 @@ async def test_bounding_box_visualization_block() -> None:
     # given
     block = BoundingBoxVisualizationBlock()
 
+    start_image = np.zeros((1000, 1000, 3), dtype=np.uint8)
     output = await block.run(
         image=WorkflowImageData(
             parent_metadata=ImageParentMetadata(parent_id="some"),
-            numpy_image=np.zeros((1000, 1000, 3), dtype=np.uint8),
+            numpy_image=start_image,
         ),
         predictions=sv.Detections(
             xyxy=np.array(
@@ -92,4 +93,46 @@ async def test_bounding_box_visualization_block() -> None:
     assert output.get("image").numpy_image.shape == (1000, 1000, 3)
     # check if the image is modified
     assert not np.array_equal(output.get("image").numpy_image, np.zeros((1000, 1000, 3), dtype=np.uint8))
+
+    # check that the image is copied
+    assert output.get("image").numpy_image.__array_interface__['data'][0] != start_image.__array_interface__['data'][0]
     
+@pytest.mark.asyncio
+async def test_bounding_box_visualization_block_nocopy() -> None:
+    # given
+    block = BoundingBoxVisualizationBlock()
+
+    start_image = np.zeros((1000, 1000, 3), dtype=np.uint8)
+    output = await block.run(
+        image=WorkflowImageData(
+            parent_metadata=ImageParentMetadata(parent_id="some"),
+            numpy_image=start_image,
+        ),
+        predictions=sv.Detections(
+            xyxy=np.array(
+                [[0, 0, 20, 20], [80, 80, 120, 120], [450, 450, 550, 550]], dtype=np.float64
+            ),
+            class_id=np.array([1, 1, 1]),
+        ),
+        copy_image=False,
+        color_palette="DEFAULT",
+        palette_size=10,
+        custom_colors=None,
+        color_axis="CLASS",
+        thickness=1,
+        roundness=0,
+    )
+
+    print("output", output)
+
+    assert output is not None
+    assert "image" in output
+    assert hasattr(output.get("image"), "numpy_image")
+    
+    # dimensions of output match input
+    assert output.get("image").numpy_image.shape == (1000, 1000, 3)
+    # check if the image is modified
+    assert not np.array_equal(output.get("image").numpy_image, np.zeros((1000, 1000, 3), dtype=np.uint8))
+
+    # check if the image reference references the same memory space as the start_image
+    assert output.get("image").numpy_image.__array_interface__['data'][0] == start_image.__array_interface__['data'][0]
