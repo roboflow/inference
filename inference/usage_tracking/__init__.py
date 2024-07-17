f672974c5c8f5ec1573399201dd8ce3400ec5957
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-07-17 19:09:37 +0200

    Incorporate comments

--- inference/usage_tracking/__init__.py
+++ inference/usage_tracking/__init__.py
@@ -1,9 +1,5 @@
 """
-In order to enable Roboflow to perform our business functions
-we need to be able to measure how our deployments are used.
-
-Users who don't use API key and who don't use premium or enterprise workflow blocks
-can turn the telemetry off by setting TELEMETRY_OPT_OUT environment variable to True.
-
-Please consult the license for more information regarding when telemetry cannot be turned off.
+Inference utilizes Roboflow's cloud services and requires telemetry during deployment.
+Customers with an offline deployment can turn the telemetry off by setting TELEMETRY_OPT_OUT environment variable to True.
+For more information please consult our licensing page [roboflow.com/licensing] or contact sales [roboflow.com/sales].
 """
