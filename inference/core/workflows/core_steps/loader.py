91496b72a6dcc639dc0dadbd8134df6d0d74843b
Thomas Hansen <thomas.hansen@gmail.com>
2024-08-15 11:35:17 -0500

    sam2 block (#563)
    * scafold sam2 block
    
    * add integration test for sam2 block
    
    * make style
    
    * add SegmentAnything2Block to list of loaded blocks
    
    * sam block: convert sam masks to instance seg response
    
    * fix sam2 maks return dimension
    
    * rename sam2 example file
    
    * None as defult point prompts on sam2 inference request
    
    * rework sam2 prompts
    
    * adjust sam2 workflow block to new sam2 prompt / response types
    
    * replace accidentaly commitded API key (key has been invalidated)
    
    * add optional boxes param to sam2 block
    
    * Use unofficial samv2 package
    
    * Pin samv2
    
    * Style
    
    * Remove wrong comment
    
    * Sam2 dockerfile changes
    
    * Change sam2 github workfloww
    
    * Upgrade pip
    
    * Remove no-cache-dir
    
    * Update docs
    
    * Install package on big runner
    
    * Style
    
    * Fix merge
    
    * Fix imports
    
    * versioning changes for Segment Anything block
    
    * forward class names from object detection boxes to sam predictions
    
    * let user pass maskinpout or decide to use mask cache explictly
    
    * make style
    
    * add example and description to Sam2SegmentationRequest prompts fielf
    
    * set defautl sam2 model variatn to hiera_tiny
    
    * rename sam2 block property `sam2_model` to `version`
    
    * disable SAM2 on hosted lambda
    
    * add better description to SAM2 block
    
    * add description to Sam2PromptSet prompts property
    
    * better error message on SAM2 remote execution attempt
    
    * configure docker images to enable/disable SAM2 based on target
    
    * SAM2 prompt type cleanup
    
    * update requirements
    
    * make style
    
    * remove unused code
    
    * types for convert_sam2_segmentation_response_to_inference_instances_seg_response
    
    * adjust sam2 block integration test
    
    * type image param on sam2 model class
    
    * make style
    
    * Add transformers extras
    
    * Add documentation for installing new extras
    
    * Add changes to expose more configuration options for SAM2, improve workdlow block and add tests
    
    * Fix sam2 postprocessing issues
    
    * remove unused imports
    
    * add multimask_output parameter to segment_anything block
    
    * add tests for segment anything block manifest parsing
    
    * make style
    
    * add inference test for sam2 embedding
    
    * add truck example image from sam2 notebook for inference tests
    
    * Update readme
    
    * Fix
    
    * add inference tests for sam2 segmentation
    
    * add sam requirements to model inference test github workflow
    
    * better check for  sam2 results
    
    * Revert "Add transformers extras"
    
    * remove unused comments from test file
    
    * check logits in sam2 segmentation test instead
    
    * optional type for boxes on sam block
    
    * remove mask_input from sam2
    
    ---------
    
    Co-authored-by: probicheaux <probicheaux@gmail.com>
    Co-authored-by: Peter <peter@roboflow.com>
    Co-authored-by: Paweł Pęczek <pawel@roboflow.com>
    Co-authored-by: Paweł Pęczek <146137186+PawelPeczek-Roboflow@users.noreply.github.com>

--- inference/core/workflows/core_steps/loader.py
+++ inference/core/workflows/core_steps/loader.py
@@ -41,6 +41,9 @@ from inference.core.workflows.core_steps.models.foundation.ocr.v1 import OCRMode
 from inference.core.workflows.core_steps.models.foundation.openai.v1 import (
     OpenAIBlockV1,
 )
+from inference.core.workflows.core_steps.models.foundation.segment_anything2.v1 import (
+    SegmentAnything2BlockV1,
+)
 from inference.core.workflows.core_steps.models.foundation.yolo_world.v1 import (
     YoloWorldModelBlockV1,
 )
@@ -194,6 +197,7 @@ REGISTERED_INITIALIZERS = {
 
 def load_blocks() -> List[Type[WorkflowBlock]]:
     return [
+        SegmentAnything2BlockV1,
         DetectionsConsensusBlockV1,
         ClipComparisonBlockV1,
         LMMBlockV1,
