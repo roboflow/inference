93aeacb57488c2b1d196b19af5545b34dadf9526
Nick Herrig <neherrig@gmail.com>
2024-08-19 21:48:59 -0500

    Remove dev files

--- watch-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# PROJECT=roboflow-platform watchmedo auto-restart --pattern="*.py" --recursive -- uvicorn cpu_http:app --port 3000
-PROJECT=roboflow-platform watchmedo auto-restart --pattern="*.py" --recursive -- uvicorn cpu_http:app --port 3000
