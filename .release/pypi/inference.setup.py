6dd28ea6740fd75cdb31a5dfcf525e442d3d790a
Peter Robicheaux <peter@roboflow.com>
2024-09-10 19:37:02 +0000

    Rename requirements.pali.server.txt -> requirements.transformers.txt

--- .release/pypi/inference.setup.py
+++ .release/pypi/inference.setup.py
@@ -66,7 +66,7 @@ setuptools.setup(
         "sam": read_requirements("requirements/requirements.sam.txt"),
         "waf": read_requirements("requirements/requirements.waf.txt"),
         "yolo-world": read_requirements("requirements/requirements.yolo_world.txt"),
-        "transformers": read_requirements("requirements/requirements.pali.server.txt"),
+        "transformers": read_requirements("requirements/requirements.transformers.txt"),
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
