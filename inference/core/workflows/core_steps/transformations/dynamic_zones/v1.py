fcac91c7463c40a5ae5e49e1a775bd46a5c5e14e
Chandler Supple <chandler.supple@gmail.com>
2024-09-16 13:22:48 -0500

    Added the ability to regularize the Dynamic Zone block

--- inference/core/workflows/core_steps/transformations/dynamic_zones/v1.py
+++ inference/core/workflows/core_steps/transformations/dynamic_zones/v1.py
@@ -1,4 +1,4 @@
-from typing import List, Literal, Optional, Type, Union
+from typing import List, Literal, Optional, Type, Union, Tuple
 
 import cv2 as cv
 import numpy as np
@@ -61,6 +61,14 @@ class DynamicZonesManifest(WorkflowBlockManifest):
         description="Keep simplifying polygon until number of vertices matches this number",
         examples=[4, "$inputs.vertices"],
     )
+    force_rectangle: Optional[bool] = Field(
+        default=False,
+        description="If true, enforce the polygon to be a rectangle when 4 vertices are requested.",
+    )
+    include_rectangle_details: Optional[bool] = Field(
+        default=False,
+        description="If true and force_rectangle is also true, include the height, width, and angle of the rectangle in the response.",
+    )
 
     @classmethod
     def accepts_batch_input(cls) -> bool:
@@ -77,24 +85,37 @@ class DynamicZonesManifest(WorkflowBlockManifest):
         return ">=1.0.0,<2.0.0"
 
 
-def calculate_simplified_polygon(
-    mask: np.ndarray, required_number_of_vertices: int, max_steps: int = 1000
-) -> np.array:
+def calculate_minimum_bounding_rectangle(mask: np.ndarray) -> Tuple[np.array, float, float, float]:
     contours = sv.mask_to_polygons(mask)
     largest_contour = max(contours, key=len)
 
-    # https://docs.opencv.org/4.x/d3/dc0/group__imgproc__shape.html#ga014b28e56cb8854c0de4a211cb2be656
+    rect = cv.minAreaRect(largest_contour)
+    box = cv.boxPoints(rect)
+    box = np.int0(box)
+    
+    width, height = rect[1]
+    angle = rect[2]
+    return box, width, height, angle
+
+
+def calculate_simplified_polygon(
+    mask: np.ndarray, required_number_of_vertices: int, force_rectangle: bool, max_steps: int = 1000
+) -> Union[np.array, Tuple[np.array, float, float, float]]:
+    contours = sv.mask_to_polygons(mask)
+    largest_contour = max(contours, key=len)
+
+    if force_rectangle and required_number_of_vertices == 4:
+        return calculate_minimum_bounding_rectangle(mask)
+
     convex_contour = cv.convexHull(
         points=largest_contour,
         returnPoints=True,
         clockwise=True,
     )
-    # https://docs.opencv.org/4.9.0/d3/dc0/group__imgproc__shape.html#ga8d26483c636be6b35c3ec6335798a47c
     perimeter = cv.arcLength(curve=convex_contour, closed=True)
     upper_epsilon = perimeter
     lower_epsilon = 0.0000001
     epsilon = lower_epsilon + upper_epsilon / 2
-    # https://docs.opencv.org/4.9.0/d3/dc0/group__imgproc__shape.html#ga0012a5fdaea70b8a9970165d98722b4c
     simplified_polygon = cv.approxPolyDP(
         curve=convex_contour, epsilon=epsilon, closed=True
     )
@@ -123,6 +144,8 @@ class DynamicZonesBlockV1(WorkflowBlock):
         self,
         predictions: Batch[sv.Detections],
         required_number_of_vertices: int,
+        force_rectangle: bool = False,
+        include_rectangle_details: bool = False,
     ) -> BlockResult:
         result = []
         for detections in predictions:
@@ -137,9 +160,22 @@ class DynamicZonesBlockV1(WorkflowBlock):
                 simplified_polygon = calculate_simplified_polygon(
                     mask=mask,
                     required_number_of_vertices=required_number_of_vertices,
+                    force_rectangle=force_rectangle,
                 )
-                if len(simplified_polygon) != required_number_of_vertices:
-                    continue
-                simplified_polygons.append(simplified_polygon)
+                if force_rectangle and required_number_of_vertices == 4:
+                    polygon, width, height, angle = simplified_polygon
+                    if include_rectangle_details:
+                        simplified_polygons.append({
+                            "polygon": polygon,
+                            "width": width,
+                            "height": height,
+                            "angle": angle
+                        })
+                    else:
+                        simplified_polygons.append(polygon)
+                else:
+                    if len(simplified_polygon) != required_number_of_vertices:
+                        continue
+                    simplified_polygons.append(simplified_polygon)
             result.append({OUTPUT_KEY: simplified_polygons})
         return result
