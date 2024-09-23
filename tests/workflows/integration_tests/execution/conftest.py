fa335d48e24fab969c18518afa367d9daf13ba85
Paweł Pęczek <pawel@roboflow.com>
2024-09-23 10:37:14 +0200

    Add more tests for Florence2 model

--- tests/workflows/integration_tests/execution/conftest.py
+++ tests/workflows/integration_tests/execution/conftest.py
@@ -59,3 +59,9 @@ def left_scissors_right_scissors() -> np.ndarray:
     return cv2.imread(
         os.path.join(ROCK_PAPER_SCISSORS_ASSETS, "left_scissors_right_scissors.jpg")
     )
+
+
+def bool_env(val):
+    if isinstance(val, bool):
+        return val
+    return val.lower() in ["true", "1", "t", "y", "yes"]
