755fd2c64e3d6eb92d920e2a75f663d0915f47f4
Peter Robicheaux <peter@roboflow.com>
2024-09-30 23:54:36 +0000

    Remove clear cache

--- inference/models/yolov11/yolov11_keypoints_detection.py
+++ inference/models/yolov11/yolov11_keypoints_detection.py
@@ -1,5 +1,4 @@
 from inference.models.yolov8.yolov8_keypoints_detection import YOLOv8KeypointsDetection
 
 class YOLOv11KeypointsDetection(YOLOv8KeypointsDetection):
-    def clear_cache(self) -> None:
-        pass
+    pass
