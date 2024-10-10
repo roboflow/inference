34c65597f8b5dda041f9aa330907a34224699551
Eddie Ramirez <eddie@roboflow.com>
2024-10-10 11:51:41 -0700

    chore: remove unused imports

--- inference/core/workflows/core_steps/classical_cv/distance_measurement/v1.py
+++ inference/core/workflows/core_steps/classical_cv/distance_measurement/v1.py
@@ -1,15 +1,12 @@
 from typing import Dict, List, Literal, Optional, Tuple, Type, Union
 
 import supervision as sv
-from pydantic import AliasChoices, ConfigDict, Field
+from pydantic import ConfigDict, Field
 
-from inference.core.workflows.execution_engine.constants import DETECTION_ID_KEY
 from inference.core.workflows.execution_engine.entities.base import (
-    Batch,
     OutputDefinition,
 )
 from inference.core.workflows.execution_engine.entities.types import (
-    DICTIONARY_KIND,
     INTEGER_KIND,
     FLOAT_KIND,
     INSTANCE_SEGMENTATION_PREDICTION_KIND,
