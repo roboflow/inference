721f04aafe83a62b3f1eb661cff1a02760dcf758
Paweł Pęczek <pawel@roboflow.com>
2024-10-15 16:22:04 +0200

    WIP - refactor of CSV sink

--- inference/core/workflows/core_steps/formatters/csv/v1.py
+++ inference/core/workflows/core_steps/formatters/csv/v1.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import Any, Dict, List, Literal, Optional, Type, Union
 
 import pandas as pd
-from pydantic import ConfigDict, Field, field_validator
+from pydantic import ConfigDict, Field, field_validator, model_validator
 
 from inference.core.workflows.core_steps.common.query_language.entities.operations import (
     AllOperationsType,
@@ -16,7 +16,7 @@ from inference.core.workflows.execution_engine.entities.types import (
     STRING_KIND,
     StepOutputSelector,
     WorkflowImageSelector,
-    WorkflowParameterSelector,
+    WorkflowParameterSelector, INTEGER_KIND, BOOLEAN_KIND,
 )
 from inference.core.workflows.prototypes.block import (
     BlockResult,
@@ -25,10 +25,17 @@ from inference.core.workflows.prototypes.block import (
 )
 
 LONG_DESCRIPTION = """
-Creates CSV files with specified columns aggregating data over specified interval. 
-"""
+The **CSV Formatter** block allows you to format and output data as a CSV file within a Workflow. 
+The configuration options provide flexibility in terms of how and when the CSV file is produced, 
+as well as how data is processed and organized. 
 
-SHORT_DESCRIPTION = "Creates CSV files with specified columns aggregating data over specified interval. "
+
+### When the CSV file is produced?
+
+CSV files usually contain multiple rows with data, whereas Workflow execution would usually 
+produce one or few rows in the CSV file. It may be needed 
+
+"""
 
 
 class BlockManifest(WorkflowBlockManifest):
@@ -36,13 +43,30 @@ class BlockManifest(WorkflowBlockManifest):
         json_schema_extra={
             "name": "CSV Formatter",
             "version": "v1",
-            "short_description": SHORT_DESCRIPTION,
+            "short_description": "Creates CSV files with specified columns.",
             "long_description": LONG_DESCRIPTION,
             "license": "Apache-2.0",
             "block_type": "formatter",
         }
     )
     type: Literal["roboflow_core/csv_formatter@v1"]
+    produces_csv_on: Literal["each_datapoint", "interval"] = Field(
+        default="each_datapoint",
+        description="Specifies how frequently block output CSV file is yielded.",
+        json_schema_extra={
+            "values_metadata": {
+                "each_datapoint": {
+                    "name": "Each Datapoint",
+                    "description": "Produces CSV file each time new input data is provided. "
+                                   "In this mode, rows are discarded when max size of CSV file is reached.",
+                },
+                "interval": {
+                    "name": "Interval",
+                    "description": "Produces CSV file on a specified time interval.",
+                },
+            }
+        },
+    )
     columns_data: Dict[
         str,
         Union[WorkflowImageSelector, WorkflowParameterSelector(), StepOutputSelector()],
@@ -66,8 +90,43 @@ class BlockManifest(WorkflowBlockManifest):
         ],
         default_factory=lambda: {},
     )
-    interval: int = Field(
-        description="Number of minutes to trigger output flush - 0 means on each prediction",
+    max_rows: Optional[Union[int, WorkflowParameterSelector(kind=[INTEGER_KIND])]] = Field(
+        default=None,
+        description="Specifies the maximum number of rows to keep in memory",
+        gt=0,
+        json_schema_extra={
+            "relevant_for": {
+                "produces_csv_on": {
+                    "values": ["each_datapoint"],
+                    "required": True,
+                },
+            }
+        },
+    )
+    flush_interval: Optional[Union[int, WorkflowParameterSelector(kind=INTEGER_KIND)]] = Field(
+        default=None,
+        description="Specifies the interval for producing CSV file in the output.",
+        gt=0,
+        json_schema_extra={
+            "relevant_for": {
+                "produces_csv_on": {
+                    "values": ["interval"],
+                    "required": True,
+                },
+            }
+        },
+    )
+    interval_unit: Optional[Literal["seconds", "minutes", "hours"]] = Field(
+        default=None,
+        description="Specifies interval unit",
+        json_schema_extra={
+            "relevant_for": {
+                "produces_csv_on": {
+                    "values": ["interval"],
+                    "required": True,
+                },
+            }
+        },
     )
 
     @field_validator("columns_data", "columns_operations")
@@ -79,13 +138,36 @@ class BlockManifest(WorkflowBlockManifest):
             )
         return value
 
+    @model_validator(mode="after")
+    def validate(self) -> "BlockManifest":
+        if self.output_production_mode == "on_each_datapoint" and self.max_rows is None:
+            raise ValueError(
+                "`max_rows` must be specified when `output_production_mode` is chosen to be `on_each_datapoint`."
+            )
+        if self.flush_interval is None or self.interval_unit is None:
+            raise ValueError(
+                "`flush_interval` and `interval_unit` must be specified when "
+                "`output_production_mode` is chosen to be `on_interval`."
+            )
+        return self
+
     @classmethod
     def describe_outputs(cls) -> List[OutputDefinition]:
-        return [OutputDefinition(name="csv_content", kind=[STRING_KIND])]
+        return [
+            OutputDefinition(name="csv_content", kind=[STRING_KIND]),
+            OutputDefinition(name="flushed", kind=[BOOLEAN_KIND])
+        ]
 
     @classmethod
     def get_execution_engine_compatibility(cls) -> Optional[str]:
         return ">=1.0.0,<2.0.0"
+
+
+INTERVAL_UNIT_TO_SECONDS = {
+    "seconds": 1,
+    "minutes": 60,
+    "hours": 60 * 60,
+}
 
 
 class CSVFormatterBlockV1(WorkflowBlock):
@@ -100,32 +182,54 @@ class CSVFormatterBlockV1(WorkflowBlock):
 
     def run(
         self,
+        produces_csv_on: Literal["each_datapoint", "interval"],
         columns_data: Dict[str, Any],
         columns_operations: Dict[str, List[AllOperationsType]],
-        interval: int,
+        max_rows: Optional[int],
+        flush_interval: Optional[int],
+        interval_unit: Optional[Literal["seconds", "minutes", "hours"]],
     ) -> BlockResult:
-        columns_data = copy(columns_data)
-        for variable_name, operations in columns_operations.items():
-            operations_chain = build_operations_chain(operations=operations)
-            columns_data[variable_name] = operations_chain(
-                columns_data[variable_name], global_parameters={}
-            )
-        columns_data["timestamp"] = datetime.now().isoformat()
-        print("Appending column data", columns_data)
-        self._buffer.append(columns_data)
-        print("DEBUG", (datetime.now() - self._last_flush_timestamp).total_seconds())
-        minutes_since_last_flush = (
-            datetime.now() - self._last_flush_timestamp
-        ).total_seconds() // 60
-        if minutes_since_last_flush >= interval:
+        csv_row = prepare_csv_row(columns_data=columns_data, columns_operations=columns_operations)
+        self._buffer.append(csv_row)
+        if produces_csv_on == "each_datapoint":
+            ensure_value_provided(value=max_rows, error_message="`max_rows` must be provided.")
             csv_content = to_csv(data=self._buffer)
-            self._buffer = []
-            self._last_flush_timestamp = datetime.now()
-            print("Flushing CSV content")
-            return {"csv_content": csv_content}
-        print(f"To flush: {interval -  minutes_since_last_flush} minutes")
-        return {"csv_content": None}
+            flushed = False
+            if len(self._buffer) >= flush_interval:
+                self._buffer = []
+                self._last_flush_timestamp = datetime.now()
+                flushed = True
+            return {"csv_content": csv_content, "flushed": flushed}
+        ensure_value_provided(value=flush_interval, error_message="`flush_interval` must be provided.")
+        ensure_value_provided(value=interval_unit, error_message="`interval_unit` must be provided.")
+        second_since_last_flush = datetime.now() - self._last_flush_timestamp
+        interval_seconds = flush_interval * INTERVAL_UNIT_TO_SECONDS[interval_unit]
+        if second_since_last_flush < interval_seconds:
+            return {"csv_content": None, "flushed": False}
+        csv_content = to_csv(data=self._buffer)
+        self._buffer = []
+        self._last_flush_timestamp = datetime.now()
+        return {"csv_content": csv_content, "flushed": True}
+
+
+def prepare_csv_row(
+    columns_data: Dict[str, Any],
+    columns_operations: Dict[str, List[AllOperationsType]],
+) -> dict:
+    columns_data = copy(columns_data)
+    for variable_name, operations in columns_operations.items():
+        operations_chain = build_operations_chain(operations=operations)
+        columns_data[variable_name] = operations_chain(
+            columns_data[variable_name], global_parameters={}
+        )
+    columns_data["timestamp"] = datetime.now().isoformat()
+    return columns_data
 
 
 def to_csv(data: List[Dict[str, Any]]) -> str:
     return pd.DataFrame(data).to_csv(index=False)
+
+
+def ensure_value_provided(value: Optional[Any], error_message: str) -> None:
+    if value is None:
+        raise ValueError(error_message)
