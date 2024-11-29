6988a360589d4e5b041475d84a6eb562f30efffd
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-11-29 11:10:09 +0100

    Store enterprise blocks in enterprise_blocks

--- inference/enterprise/workflows/enterprise_blocks/loader.py
+++ inference/enterprise/workflows/enterprise_blocks/loader.py
@@ -1,7 +1,7 @@
 from typing import List, Type
 
 from inference.core.workflows.prototypes.block import WorkflowBlock
-from inference.enterprise.workflows.enterprise_steps.sinks.opc_writer.v1 import (
+from inference.enterprise.workflows.enterprise_blocks.sinks.opc_writer.v1 import (
     OPCWriterSinkBlockV1,
 )
 
