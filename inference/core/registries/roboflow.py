4e986392b0b39611d6816d8bb88b866f8edeb80b
Balthasar <balthasar@roboflow.com>
2024-12-18 09:26:43 +0100

    cleaning up the registry

--- inference/core/registries/roboflow.py
+++ inference/core/registries/roboflow.py
@@ -3,7 +3,7 @@ from typing import Optional, Tuple, Union
 
 from inference.core.cache import cache
 from inference.core.devices.utils import GLOBAL_DEVICE_ID
-from inference.core.entities.types import DatasetID, ModelType, TaskType, VersionID
+from inference.core.entities.types import ModelType, TaskType
 from inference.core.env import LAMBDA, MODEL_CACHE_DIR
 from inference.core.exceptions import (
     MissingApiKeyError,
@@ -90,52 +90,47 @@ def get_model_type(
     """
     model_id = resolve_roboflow_model_alias(model_id=model_id)
     dataset_id, version_id = get_model_id_chunks(model_id=model_id)
+    lock_key, cache_path = determine_cache_paths(dataset_or_model_id=dataset_id, version_id=version_id)
+    
     if dataset_id in GENERIC_MODELS:
         logger.debug(f"Loading generic model: {dataset_id}.")
         return GENERIC_MODELS[dataset_id]
     
-    if version_id is None:
-        print("it's a new model id")
-        api_data = get_roboflow_model_data(
-            api_key=api_key,
-            model_id=model_id,
-            endpoint_type=ModelEndpointType.ORT,
-            device_id=GLOBAL_DEVICE_ID,
-        ).get("ort")
-        print("api data from new model id", api_data)
-        return (api_data.get("taskType"), api_data.get("modelType"))
-   
     cached_metadata = get_model_metadata_from_cache(
-        dataset_id=dataset_id, version_id=version_id
+        cache_path=cache_path, lock_key=lock_key
     )
     if cached_metadata is not None:
         print("returning cached_metadata", cached_metadata)
         return cached_metadata[0], cached_metadata[1]
+    print("not cached metadata")
+    workspace_id = get_roboflow_workspace(api_key=api_key)
+    
+    # THis path will never be executed for a model ID
     if version_id == STUB_VERSION_ID:
         if api_key is None:
             raise MissingApiKeyError(
                 "Stub model version provided but no API key was provided. API key is required to load stub models."
             )
-        workspace_id = get_roboflow_workspace(api_key=api_key)
+        
         project_task_type = get_roboflow_dataset_type(
             api_key=api_key, workspace_id=workspace_id, dataset_id=dataset_id
         )
         model_type = "stub"
         save_model_metadata_in_cache(
-            dataset_id=dataset_id,
-            version_id=version_id,
+            cache_path=cache_path,
+            lock_key=lock_key,
             project_task_type=project_task_type,
             model_type=model_type,
         )
         return project_task_type, model_type
-    workspace_id = get_roboflow_workspace(api_key=api_key)
+
     api_data = get_roboflow_model_data(
         api_key=api_key,
         model_id=model_id,
         endpoint_type=ModelEndpointType.ORT,
         device_id=GLOBAL_DEVICE_ID,
     ).get("ort")
-    print("api_data", api_data)
+
     if api_data is None:
         raise ModelArtefactError("Error loading model artifacts from Roboflow API.")
     # some older projects do not have type field - hence defaulting
@@ -148,48 +143,47 @@ def get_model_type(
     if model_type is None or project_task_type is None:
         raise ModelArtefactError("Error loading model artifacts from Roboflow API.")
     save_model_metadata_in_cache(
-        dataset_id=dataset_id,
-        version_id=version_id,
+        cache_path=cache_path,
+        lock_key=lock_key,
         project_task_type=project_task_type,
         model_type=model_type,
     )
-
     print("returning project_task_type and model_type", project_task_type, model_type)
-
     return project_task_type, model_type
 
+def determine_cache_paths(dataset_or_model_id: str, version_id: Optional[str]) -> Tuple[str, str]:    
+    if dataset_or_model_id and version_id:
+        # It's a dataset/version ID
+        lock_key = f"lock:metadata:dataset:{dataset_or_model_id}:{version_id}"
+        cache_path = construct_dataset_version_cache_path(dataset_or_model_id, version_id)
+    else:
+        # It's a model ID
+        lock_key = f"lock:metadata:model:{dataset_or_model_id}"
+        cache_path = construct_model_id_cache_path(dataset_or_model_id)
+    
+    return lock_key, cache_path
 
 def get_model_metadata_from_cache(
-    dataset_id: str, version_id: str
+    cache_path: str,
+    lock_key: str
 ) -> Optional[Tuple[TaskType, ModelType]]:
     if LAMBDA:
-        return _get_model_metadata_from_cache(
-            dataset_id=dataset_id, version_id=version_id
-        )
-    with cache.lock(
-        f"lock:metadata:{dataset_id}:{version_id}", expire=CACHE_METADATA_LOCK_TIMEOUT
-    ):
-        return _get_model_metadata_from_cache(
-            dataset_id=dataset_id, version_id=version_id
-        )
+        return _get_model_metadata_from_cache(cache_path=cache_path)
+    
+    with cache.lock(lock_key, expire=CACHE_METADATA_LOCK_TIMEOUT):
+        return _get_model_metadata_from_cache(cache_path=cache_path)
 
-
-def _get_model_metadata_from_cache(
-    dataset_id: str, version_id: str
-) -> Optional[Tuple[TaskType, ModelType]]:
-    model_type_cache_path = construct_model_type_cache_path(
-        dataset_id=dataset_id, version_id=version_id
-    )
-    if not os.path.isfile(model_type_cache_path):
+def _get_model_metadata_from_cache(cache_path: str) -> Optional[Tuple[TaskType, ModelType]]:
+    if not os.path.isfile(cache_path):
         return None
     try:
-        model_metadata = read_json(path=model_type_cache_path)
+        model_metadata = read_json(path=cache_path)
         if model_metadata_content_is_invalid(content=model_metadata):
             return None
         return model_metadata[PROJECT_TASK_TYPE_KEY], model_metadata[MODEL_TYPE_KEY]
     except ValueError as e:
         logger.warning(
-            f"Could not load model description from cache under path: {model_type_cache_path} - decoding issue: {e}."
+            f"Could not load model description from cache under path: {cache_path} - decoding issue: {e}."
         )
         return None
 
@@ -210,49 +204,45 @@ def model_metadata_content_is_invalid(content: Optional[Union[list, dict]]) -> b
 
 
 def save_model_metadata_in_cache(
-    dataset_id: DatasetID,
-    version_id: VersionID,
+    cache_path: str,
+    lock_key: str,
     project_task_type: TaskType,
     model_type: ModelType,
 ) -> None:
     if LAMBDA:
         _save_model_metadata_in_cache(
-            dataset_id=dataset_id,
-            version_id=version_id,
+            cache_path=cache_path,
             project_task_type=project_task_type,
             model_type=model_type,
         )
         return None
-    with cache.lock(
-        f"lock:metadata:{dataset_id}:{version_id}", expire=CACHE_METADATA_LOCK_TIMEOUT
-    ):
+    
+    with cache.lock(lock_key, expire=CACHE_METADATA_LOCK_TIMEOUT):
         _save_model_metadata_in_cache(
-            dataset_id=dataset_id,
-            version_id=version_id,
+            cache_path=cache_path,
             project_task_type=project_task_type,
             model_type=model_type,
         )
         return None
-
 
 def _save_model_metadata_in_cache(
-    dataset_id: DatasetID,
-    version_id: VersionID,
+    cache_path: str,
     project_task_type: TaskType,
     model_type: ModelType,
 ) -> None:
-    model_type_cache_path = construct_model_type_cache_path(
-        dataset_id=dataset_id, version_id=version_id
-    )
     metadata = {
         PROJECT_TASK_TYPE_KEY: project_task_type,
         MODEL_TYPE_KEY: model_type,
     }
+    print("saving metadata to cache", cache_path, metadata)
     dump_json(
-        path=model_type_cache_path, content=metadata, allow_override=True, indent=4
+        path=cache_path, content=metadata, allow_override=True, indent=4
     )
 
+def construct_model_id_cache_path(model_id: str) -> str:
+    """Constructs the cache path for a given model ID."""
+    return os.path.join(MODEL_CACHE_DIR, "models", model_id, "model_type.json")
 
-def construct_model_type_cache_path(dataset_id: str, version_id: str) -> str:
-    cache_dir = os.path.join(MODEL_CACHE_DIR, dataset_id, version_id)
-    return os.path.join(cache_dir, "model_type.json")
+def construct_dataset_version_cache_path(dataset_id: str, version_id: str) -> str:
+    """Constructs the cache path for a given dataset ID and version ID."""
+    return os.path.join(MODEL_CACHE_DIR, dataset_id, version_id, "model_type.json")
