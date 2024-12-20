ae13e67850fd63629371750a0fd91533f36cae48
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2024-12-20 15:02:53 +0100

    formatting

--- inference/usage_tracking/collector.py
+++ inference/usage_tracking/collector.py
@@ -348,9 +348,7 @@ class UsageCollector:
             source_usage["source_duration"] += (
                 frames / fps if fps and not inference_test_run else 0
             )
-            source_usage["fps"] = (
-                fps if isinstance(fps, numbers.Number) else 0
-            )
+            source_usage["fps"] = fps if isinstance(fps, numbers.Number) else 0
             source_usage["category"] = category
             source_usage["resource_id"] = resource_id
             source_usage["resource_details"] = json.dumps(resource_details)
