7f86a1380fc75237b08fc7ae7a609932b58e856d
Brad Dwyer <brad@roboflow.com>
2025-01-04 11:27:18 -0600

    Fix style

--- inference/core/workflows/core_steps/loader.py
+++ inference/core/workflows/core_steps/loader.py
@@ -390,6 +390,7 @@ from inference.core.workflows.execution_engine.entities.types import (
     CONTOURS_KIND,
     DETECTION_KIND,
     DICTIONARY_KIND,
+    EMBEDDING_KIND,
     FLOAT_KIND,
     FLOAT_ZERO_TO_ONE_KIND,
     IMAGE_KEYPOINTS_KIND,
@@ -418,7 +419,6 @@ from inference.core.workflows.execution_engine.entities.types import (
     VIDEO_METADATA_KIND,
     WILDCARD_KIND,
     ZONE_KIND,
-    EMBEDDING_KIND,
     Kind,
 )
 from inference.core.workflows.prototypes.block import WorkflowBlock
