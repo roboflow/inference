5d33b6ebd9e4c14effffd2bfcd1a7f7855d0ec4a
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2025-01-10 15:34:53 +0100

    Use mqtt broker mock instead of external service

--- tests/workflows/integration_tests/execution/test_workflow_with_mqtt_writer.py
+++ tests/workflows/integration_tests/execution/test_workflow_with_mqtt_writer.py
@@ -1,51 +1,32 @@
 import unittest
-import time
 import paho.mqtt.client as mqtt
 from inference.enterprise.workflows.enterprise_blocks.sinks.mqtt_writer.v1 import MQTTWriterSinkBlockV1
 import pytest
+import threading
 
 @pytest.mark.timeout(5)
-class MQTTWriterSinkBlockV1IntegrationTest(unittest.TestCase):
-    def setUp(self):
-        self.broker_host = 'test.mosquitto.org'
-        self.broker_port = 1883
-        self.topic = 'test/topic'
-        self.received_messages = []
+def test_successful_connection_and_publishing(fake_mqtt_broker):
+    # given
+    block = MQTTWriterSinkBlockV1()
+    published_message = 'Test message'
+    expected_message = 'Message published successfully'
 
-        self.listener_client = mqtt.Client()
-        self.listener_client.on_message = self.on_message
-        self.listener_client.connect(self.broker_host, self.broker_port)
-        self.listener_client.subscribe(self.topic)
-        self.listener_client.loop_start()
+    fake_mqtt_broker.messages_count_to_wait_for = 2
+    broker_thread = threading.Thread(target=fake_mqtt_broker.start)
+    broker_thread.start()
 
-    def on_message(self, client, userdata, msg):
-        self.received_messages.append(msg.payload.decode())
+    # when
+    result = block.run(
+        host=fake_mqtt_broker.host,
+        port=fake_mqtt_broker.port,
+        topic="RoboflowTopic",
+        message=published_message
+    )
 
-    def tearDown(self):
-        self.listener_client.loop_stop()
-        self.listener_client.disconnect()
+    broker_thread.join(timeout=2)
 
-    def test_successful_connection_and_publishing(self):
-        # given
-        block = MQTTWriterSinkBlockV1()
-        message = 'Test message'
-        expected_error_status = False
-        expected_message = 'Message published successfully'
+    # then
+    assert result['error_status'] is False, "No error expected"
+    assert result['message'] == expected_message
 
-        # when
-        result = block.run(
-            host=self.broker_host,
-            port=self.broker_port,
-            topic=self.topic,
-            message=message
-        )
-
-        time.sleep(2)
-
-        # then
-        self.assertEqual(result['error_status'], expected_error_status)
-        self.assertEqual(result['message'], expected_message)
-        self.assertIn(message, self.received_messages)
-
-if __name__ == '__main__':
-    unittest.main() 
+    assert published_message.encode() in fake_mqtt_broker.messages[-1]
