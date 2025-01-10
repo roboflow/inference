174e84e5563c87811583417d503cff15e57fb417
Chandler Supple <chandler.supple@gmail.com>
2025-01-09 19:06:33 -0800

    Persisted the MQTT connection

--- inference/enterprise/workflows/enterprise_blocks/sinks/mqtt_writer/v1.py
+++ inference/enterprise/workflows/enterprise_blocks/sinks/mqtt_writer/v1.py
@@ -25,7 +25,7 @@ class BlockManifest(WorkflowBlockManifest):
             "version": "v1",
             "short_description": "Publishes messages to an MQTT broker.",
             "long_description": "This block allows publishing messages to a specified MQTT broker and topic.",
-            "license": "Apache-2.0",
+            "license": "Roboflow Enterprise License",
             "block_type": "sink",
         }
     )
@@ -56,13 +56,29 @@ class BlockManifest(WorkflowBlockManifest):
         description="Whether the message should be retained by the broker.",
         examples=[True, False],
     )
+    username: Union[Selector(kind=[STRING_KIND]), str] = Field(
+        default=None,
+        description="Username for MQTT broker authentication.",
+        examples=["$inputs.mqtt_username"],
+    )
+    password: Union[Selector(kind=[STRING_KIND]), str] = Field(
+        default=None,
+        description="Password for MQTT broker authentication.",
+        examples=["$inputs.mqtt_password"],
+    )
 
     @classmethod
     def describe_outputs(cls) -> List[OutputDefinition]:
-        return [OutputDefinition(name="status", kind=[STRING_KIND])]
+        return [
+            OutputDefinition(name="error_status", kind=[BOOLEAN_KIND]),
+            OutputDefinition(name="message", kind=[STRING_KIND])
+        ]
 
 
 class MQTTWriterSinkBlockV1(WorkflowBlock):
+    def __init__(self):
+        self.mqtt_client: Optional[mqtt.Client] = mqtt.Client()
+
     @classmethod
     def get_manifest(cls) -> Type[WorkflowBlockManifest]:
         return BlockManifest
@@ -73,15 +89,37 @@ class MQTTWriterSinkBlockV1(WorkflowBlock):
         port: int,
         topic: str,
         message: str,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
         qos: int = 0,
         retain: bool = False,
     ) -> BlockResult:
-        client = mqtt.Client()
+        if self.mqtt_client is None:
+            self.mqtt_client = mqtt.Client()
+            if username and password:
+                self.mqtt_client.username_pw_set(username, password)
+            self.mqtt_client.on_connect = self.mqtt_on_connect
+            self.mqtt_client.on_connect_fail = self.mqtt_on_connect_fail
+
+        if not self.mqtt_client.is_connected():
+            try:
+                self.mqtt_client.connect(host, port)
+            except Exception as e:
+                logging.error(f"Failed to connect to MQTT broker: {e}")
+                return {"error_status": True, "message": f"Failed to connect to MQTT broker: {e}"}
+
         try:
-            client.connect(host, port)
-            client.publish(topic, message, qos=qos, retain=retain)
-            client.disconnect()
-            return {"status": "Message published successfully"}
+            res: mqtt.MQTTMessageInfo = self.mqtt_client.publish(topic, message, qos=qos, retain=retain)
+            if res.is_published():
+                return {"error_status": False, "message": "Message published successfully"}
+            else:
+                return {"error_status": True, "message": "Failed to publish payload"}
         except Exception as e:
             logging.error(f"Failed to publish message: {e}")
-            return {"status": f"Failed to publish message: {e}"}
+            return {"error_status": True, "message": f"Unhandled error - {e}"}
+
+    def mqtt_on_connect(self, client, userdata, flags, reason_code, properties=None):
+        logging.info(f"Connected with result code {reason_code}")
+
+    def mqtt_on_connect_fail(self, client, userdata, flags, reason_code, properties=None):
+        logging.error(f"Failed to connect with result code {reason_code}")
