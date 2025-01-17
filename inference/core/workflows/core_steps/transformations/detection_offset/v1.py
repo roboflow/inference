2d5d055e7e9fc2f269a964f06b07cda5b68786e8
Chandler Supple <chandler.supple@gmail.com>
2025-01-17 07:22:07 -0800

    Made the linter happy

--- inference/core/workflows/core_steps/transformations/detection_offset/v1.py
+++ inference/core/workflows/core_steps/transformations/detection_offset/v1.py
@@ -150,8 +150,14 @@ def offset_detections(
                 (
                     max(0, x1 - int(image_dimensions[i][1] * offset_width / 200)),
                     max(0, y1 - int(image_dimensions[i][0] * offset_height / 200)),
-                    min(image_dimensions[i][1], x2 + int(image_dimensions[i][1] * offset_width / 200)),
-                    min(image_dimensions[i][0], y2 + int(image_dimensions[i][0] * offset_height / 200)),
+                    min(
+                        image_dimensions[i][1],
+                        x2 + int(image_dimensions[i][1] * offset_width / 200),
+                    ),
+                    min(
+                        image_dimensions[i][0],
+                        y2 + int(image_dimensions[i][0] * offset_height / 200),
+                    ),
                 )
                 for i, (x1, y1, x2, y2) in enumerate(_detections.xyxy)
             ]
