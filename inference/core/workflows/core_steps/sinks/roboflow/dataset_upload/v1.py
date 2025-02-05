d3e7fa8cc72ed602291b29a87937f18a2b0e2b98
Emily Gavrilenko <egavrile@calpoly.edu>
2025-02-04 18:46:02 -0800

    fix import order

--- inference/core/workflows/core_steps/sinks/roboflow/dataset_upload/v1.py
+++ inference/core/workflows/core_steps/sinks/roboflow/dataset_upload/v1.py
@@ -27,7 +27,7 @@ from uuid import uuid4
 
 import supervision as sv
 from fastapi import BackgroundTasks
-from pydantic import ConfigDict, Field, AliasChoices
+from pydantic import AliasChoices, ConfigDict, Field
 
 from inference.core.active_learning.cache_operations import (
     return_strategy_credit,
