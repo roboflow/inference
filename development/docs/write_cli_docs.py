ac2c4ebbd22f30d2ce8787acd5218c984c8361dc
Thomas Hansen <thomas.hansen@gmail.com>
2025-02-19 13:40:59 -0600

    disable writing cli docs to debug docs build

--- development/docs/write_cli_docs.py
+++ development/docs/write_cli_docs.py
@@ -10,15 +10,15 @@ DOCS_ROOT_DIR = os.path.abspath(
     )
 )
 
-filename = os.path.join(DOCS_ROOT_DIR, "inference_helpers", "cli_commands", "cli_reference.md")
+filename = os.path.join(DOCS_ROOT_DIR, "inference_helpers", "cli_commands", "reference.md")
 
 def main():
     cmd = f"typer inference_cli.main utils docs --name inference"
     result = subprocess.run(cmd.split(), capture_output=True, text=True)
     content = result.stdout
-    print("Writing CLI reference to ", filename)
-    with open(filename, 'w') as f:
-        f.write(content)
+    print("NOT Writing CLI reference to ", filename)
+    #with open(filename, 'w') as f:
+    #    f.write(content)
 
 
 if __name__ == "__main__":
