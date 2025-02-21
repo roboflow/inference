285f15b43051a906ae3a3314edd2624e2bdc0a3f
Chandler Supple <chandler.supple@gmail.com>
2025-02-20 22:39:43 -0800

    Fixed the import

--- inference/enterprise/workflows/enterprise_blocks/loader.py
+++ inference/enterprise/workflows/enterprise_blocks/loader.py
@@ -1,9 +1,9 @@
 from typing import List, Type
 
+from inference.core.workflows.prototypes.block import WorkflowBlock
 from inference.enterprise.workflows.enterprise_blocks.sinks.microsoft_sql_server.v1 import (
     MicrosoftSQLServerSinkBlockV1,
 )
-from inference.core.workflows.prototypes.block import WorkflowBlock
 from inference.enterprise.workflows.enterprise_blocks.sinks.mqtt_writer.v1 import (
     MQTTWriterSinkBlockV1,
 )
