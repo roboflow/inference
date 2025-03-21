8ce61bbe72b0489e3d059570b64ce9da39bb752d
Saurabh Misra <misra.saurabh1@gmail.com>
2025-03-20 19:10:29 -0700

    Install codeflash and setup the github action. Also attempt to optimize bubble sort

---
+++ tests/inference/unit_tests/core/test_bubble_sort.py
@@ -0,0 +1,11 @@
+from inference.core.bubble_sort import sorter
+
+
+def test_sort():
+    input = [5, 4, 3, 2, 1, 0]
+    output = sorter(input)
+    assert output == [0, 1, 2, 3, 4, 5]
+
+    input = [5.0, 4.0, 3.0, 2.0, 1.0, 0.0]
+    output = sorter(input)
+    assert output == [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
