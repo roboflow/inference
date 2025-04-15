d8ccd55d34acf858506a032355dd0be3ee814530
Lou Loizides <louloizides@LouisLoizidess-MacBook-Pro-LC3J6G7YXF.local>
2025-04-15 14:09:47 -0400

    Changed comments and formatting

--- inference/core/workflows/core_steps/analytics/overlap/v1.py
+++ inference/core/workflows/core_steps/analytics/overlap/v1.py
@@ -5,17 +5,13 @@ import supervision as sv
 from pydantic import ConfigDict, Field
 from typing_extensions import Literal, Type
 from supervision.detection.utils import get_data_item
-import copy
 
-from inference.core.workflows.execution_engine.constants import (
-    TIME_IN_ZONE_KEY_IN_SV_DETECTIONS,
-)
+
 from inference.core.workflows.execution_engine.entities.base import (
     OutputDefinition,
 )
 from inference.core.workflows.execution_engine.entities.types import (
     OBJECT_DETECTION_PREDICTION_KIND,
-    STRING_KIND,
     Selector,
 )
 from inference.core.workflows.prototypes.block import (
@@ -25,10 +21,26 @@ from inference.core.workflows.prototypes.block import (
 )
 
 OUTPUT_KEY: str = "overlaps"
-SHORT_DESCRIPTION = "Filter objects that overlap another class"
+SHORT_DESCRIPTION = "Filter objects overlapping some other class"
 LONG_DESCRIPTION = """
-The `OverlapFilter` is an analytics block that filters out objects overlapping on another class
+The `OverlapFilter` is an analytics block that filters out objects overlapping instances of some other class
 """
+
+
+# trims a numpy array or list to specific indices
+# after checking for None or empty
+def trim_array(arr, idx):
+    idx = list(idx)
+    if arr is None:
+        return arr
+
+    if not arr.any():
+        return arr
+
+    if isinstance(arr, np.ndarray):
+        return arr[idx]
+    else:
+        return [arr[i] for i in idx]
 
 
 class OverlapManifest(WorkflowBlockManifest):
@@ -60,9 +72,7 @@ class OverlapManifest(WorkflowBlockManifest):
         description="Overlap Type.",
         examples=["Center Overlap", "Any Overlap"],
     )
-    overlap_class_name: Union[
-        str
-    ] = Field(
+    overlap_class_name: Union[str] = Field(
         description="Overlap Class Name",
         json_schema_extra={
             "hide_description": True,
@@ -94,30 +104,22 @@ class OverlapBlockV1(WorkflowBlock):
         return OverlapManifest
 
     @classmethod
-    def coords_overlap(cls,overlap,other,overlap_type):
+    def coords_overlap(cls, overlap, other, overlap_type):
 
         # coords are [x1, y1, x2, y2]
-        if overlap_type=="Center Overlap":
-            size = [other[2]-other[0],other[3]-other[1]]
-            (x,y) = [other[0]+size[0]/2,other[1]+size[1]/2]        
-            return x>overlap[0] and x<overlap[2] and y>overlap[1] and y<overlap[3]
-        else:       
-            return not (other[2] < overlap[0] or other[0] > overlap[2] or other[3] < overlap[1] or other[1] > overlap[3])
-
-    @classmethod
-    def safe_trim(cls,arr,idx):
-        idx = list(idx)
-        if arr is None:
-            return arr
-
-        if not arr.any():
-            return arr
-            
-        trimmed_idx = [i for i in idx if i<len(arr)]
-        if isinstance(arr, np.ndarray):
-            return arr[trimmed_idx]
+        if overlap_type == "Center Overlap":
+            size = [other[2] - other[0], other[3] - other[1]]
+            (x, y) = [other[0] + size[0] / 2, other[1] + size[1] / 2]
+            return (
+                x > overlap[0] and x < overlap[2] and y > overlap[1] and y < overlap[3]
+            )
         else:
-            return [arr[i] for i in trimmed_idx]
+            return not (
+                other[2] < overlap[0]
+                or other[0] > overlap[2]
+                or other[3] < overlap[1]
+                or other[1] > overlap[3]
+            )
 
     def run(
         self,
@@ -125,31 +127,40 @@ class OverlapBlockV1(WorkflowBlock):
         overlap_type: str,
         overlap_class_name: str,
     ) -> BlockResult:
-        
+
         overlaps = []
         others = []
         for i in range(len(predictions.xyxy)):
             data = get_data_item(predictions.data, i)
-            if data["class_name"]==overlap_class_name:
+            if data["class_name"] == overlap_class_name:
                 overlaps.append(predictions.xyxy[i])
             else:
-                others.append((predictions.xyxy[i],i))
+                others.append((predictions.xyxy[i], i))
 
         idx = set()
         for overlap in overlaps:
             if not others:
                 break
-            idx = idx.union({other[1] for other in others if OverlapBlockV1.coords_overlap(overlap,other[0],overlap_type)})        
+            idx = idx.union(
+                {
+                    other[1]
+                    for other in others
+                    if OverlapBlockV1.coords_overlap(overlap, other[0], overlap_type)
+                }
+            )
             # once it's overlapped we don't need to check again
             others = [o for o in others if o[1] not in idx]
 
         # trim Detections to the filtered indices
-        filtered = copy.deepcopy(predictions)
-        filtered.xyxy = OverlapBlockV1.safe_trim(filtered.xyxy,idx)
-        filtered.mask = OverlapBlockV1.safe_trim(filtered.mask,idx)
-        filtered.confidence = OverlapBlockV1.safe_trim(filtered.confidence,idx)
-        filtered.tracker_id = OverlapBlockV1.safe_trim(filtered.tracker_id,idx)
-        filtered.class_id = OverlapBlockV1.safe_trim(filtered.class_id,idx)
-        filtered.data = {k:OverlapBlockV1.safe_trim(filtered.data[k],idx) for k in predictions.data.keys()}
-        
+        # this should be faster than iterating since each collection is copied once
+        filtered = sv.Detections.empty()
+        filtered.xyxy = trim_array(predictions.xyxy, idx)
+        filtered.mask = trim_array(predictions.mask, idx)
+        filtered.confidence = trim_array(predictions.confidence, idx)
+        filtered.tracker_id = trim_array(predictions.tracker_id, idx)
+        filtered.class_id = trim_array(predictions.class_id, idx)
+        filtered.data = {
+            k: trim_array(predictions.data[k], idx) for k in predictions.data.keys()
+        }
+
         return {OUTPUT_KEY: filtered}
