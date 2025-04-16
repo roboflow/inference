fbfe5d9cba14bc69ecfe29065d2afbbeea6fd194
Louis Loizides <206562302+lou-roboflow@users.noreply.github.com>
2025-04-16 11:14:44 -0400

    ran isort

--- inference/core/workflows/core_steps/loader.py
+++ inference/core/workflows/core_steps/loader.py
@@ -17,6 +17,7 @@ from inference.core.workflows.core_steps.analytics.line_counter.v1 import (
 from inference.core.workflows.core_steps.analytics.line_counter.v2 import (
     LineCounterBlockV2,
 )
+from inference.core.workflows.core_steps.analytics.overlap.v1 import OverlapBlockV1
 from inference.core.workflows.core_steps.analytics.path_deviation.v1 import (
     PathDeviationAnalyticsBlockV1,
 )
@@ -30,9 +31,6 @@ from inference.core.workflows.core_steps.analytics.time_in_zone.v2 import (
     TimeInZoneBlockV2,
 )
 from inference.core.workflows.core_steps.analytics.velocity.v1 import VelocityBlockV1
-from inference.core.workflows.core_steps.analytics.overlap.v1 import (
-    OverlapBlockV1,
-)
 from inference.core.workflows.core_steps.cache.cache_get.v1 import CacheGetBlockV1
 from inference.core.workflows.core_steps.cache.cache_set.v1 import CacheSetBlockV1
 from inference.core.workflows.core_steps.classical_cv.camera_focus.v1 import (
