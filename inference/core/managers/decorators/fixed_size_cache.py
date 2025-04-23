e01bc6e82e452a62c1b730c5b8a7c691ce5e26c1
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2025-04-23 22:38:17 +0200

    Extend error message

--- inference/core/managers/decorators/fixed_size_cache.py
+++ inference/core/managers/decorators/fixed_size_cache.py
@@ -49,6 +49,10 @@ class WithFixedSizeCache(ModelManagerDecorator):
             if not self._key_queue:
                 logger.error(
                     "Tried to remove model from cache even though key queue is already empty!"
+                    "(max_size: %s, len(self): %s, MEMORY_FREE_THRESHOLD: %s)",
+                    self.max_size,
+                    len(self),
+                    MEMORY_FREE_THRESHOLD,
                 )
                 break
             to_remove_model_id = self._key_queue.popleft()
