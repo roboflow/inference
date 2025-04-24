0889bef73f1e64d74f98e4587e6ded7d15c7d015
Paweł Pęczek <pawel@roboflow.com>
2025-04-24 15:33:58 +0200

    Fix sudo

--- build_scripts/compile_opencv_jetpack_6_2.sh
+++ build_scripts/compile_opencv_jetpack_6_2.sh
@@ -4,6 +4,7 @@
 version="4.10.0"
 folder="opencv_compilation"
 apt -y purge *libopencv*
+apt-get update
 apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev python3.10-dev python3-numpy
 apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libv4l-dev v4l-utils qv4l2
