4282a24d81ff77a54d5d9f91ed0a833947026932
Kevin Turcios <turcioskevinr@gmail.com>
2025-05-08 13:59:02 -0700

    add benchmark configuration and case for codeflash

---
+++ tests/inference/unit_tests/benchmarks/core/test_speed_benchmark.py
@@ -0,0 +1,35 @@
+from inference_cli.lib.benchmark.dataset import load_dataset_images
+import numpy as np
+import pytest
+
+
+from inference import get_model
+
+
+@pytest.fixture
+def dataset_reference() -> tuple[list[np.ndarray], set[tuple[int, int]]]:
+    dataset_images = load_dataset_images(
+        dataset_reference="coco",
+    )
+    return dataset_images, {i.shape[:2] for i in dataset_images}
+
+
+# args of inference benchmark python-package-speed -m yolov8n-seg-640 -bi 10000 command
+args = {
+    "model_id": "yolov8n-seg-640",
+    "dataset_reference": "coco",
+    "warm_up_inferences": 10,
+    "benchmark_inferences": 10000,
+    "batch_size": 1,
+    "api_key": None,
+    "model_configuration": None,
+    "output_location": None,
+}
+
+
+def test_benchmark_equivalent(benchmark, dataset_reference):
+    images, image_sizes = dataset_reference
+
+    model = get_model(model_id="yolov8n-seg-640", api_key=None)
+
+    benchmark(model.infer, images[0])
