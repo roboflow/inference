9d6d49fa99070452ba5c163203c68bcf88bf4644
Paweł Pęczek <pawel@roboflow.com>
2025-06-10 18:46:37 +0200

    Add build for jetpack 6

---
+++ inference_experimental/dockerfiles/build_scripts/compile_opencv_jetpack_6.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+
+version="4.10.0"
+folder="opencv_compilation"
+apt -y purge *libopencv*
+apt-get update
+apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev python3.10-dev python3-numpy
+apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libv4l-dev v4l-utils qv4l2
+apt-get install -y curl unzip
+mkdir $folder
+cd ${folder}
+curl -L https://github.com/opencv/opencv/archive/${version}.zip -o opencv-${version}.zip
+curl -L https://github.com/opencv/opencv_contrib/archive/${version}.zip -o opencv_contrib-${version}.zip
+unzip opencv-${version}.zip
+unzip opencv_contrib-${version}.zip
+rm opencv-${version}.zip opencv_contrib-${version}.zip
+cd opencv-${version}/
+mkdir release
+cd release/
+cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D CUDA_ARCH_BIN="8.7" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j$(nproc)
+make install
+echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PYTHONPATH=/usr/local/lib/python3.10/site-packages/:$PYTHONPATH' >> ~/.bashrc
+source ~/.bashrc
+cd python_loader
+python setup.py bdist_wheel
+python -m pip install dist/opencv-${version}-py3-none-any.whl
