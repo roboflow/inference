6eccabf7b60b73b70f2d0280b1117bb18abdbb71
Paweł Pęczek <pawel@roboflow.com>
2025-06-19 13:58:46 +0200

    Another attempt to make build more reprodicible

--- inference_experimental/dockerfiles/build_scripts/compile_opencv_jetpack_6.sh
+++ inference_experimental/dockerfiles/build_scripts/compile_opencv_jetpack_6.sh
@@ -27,4 +27,3 @@ echo 'export PYTHONPATH=/usr/local/lib/python3.10/site-packages/:$PYTHONPATH' >>
 source ~/.bashrc
 cd python_loader
 python setup.py bdist_wheel
-$HOME/.local/bin/uv pip install --system dist/opencv-${version}-py3-none-any.whl
