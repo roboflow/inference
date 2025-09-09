e02ce770e963c4bd61087501d16111ec269f0897
Rodrigo Barbosa <rodrigo@roboflow.com>
2025-09-09 15:37:31 -0300

    time in zone fix to handle multiple zones

--- inference/core/workflows/core_steps/analytics/time_in_zone/v2.py
+++ inference/core/workflows/core_steps/analytics/time_in_zone/v2.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
+import itertools
 import supervision as sv
 from pydantic import ConfigDict, Field
 from typing_extensions import Literal, Type
@@ -113,11 +114,19 @@ class TimeInZoneBlockV2(WorkflowBlock):
     def get_manifest(cls) -> Type[WorkflowBlockManifest]:
         return TimeInZoneManifest
 
+    def normalize_zone(self, zone: List[Tuple[int, int]] | List[List[Tuple[int, int]]]) -> List[List[Tuple[int, int]]]:
+        if len(zone) > 0 and zone[0] and isinstance(zone[0][0], (int, float, np.int32)):
+            return [zone]
+        return zone
+    
+    def flatten_list(self, iterable):
+        return list(itertools.chain.from_iterable(iterable))
+
     def run(
         self,
         image: WorkflowImageData,
         detections: sv.Detections,
-        zone: List[Tuple[int, int]],
+        zone: List[Tuple[int, int]] | List[List[Tuple[int, int]]],
         triggering_anchor: str,
         remove_out_of_zone_detections: bool,
         reset_out_of_zone_detections: bool,
@@ -127,30 +136,31 @@ class TimeInZoneBlockV2(WorkflowBlock):
                 f"tracker_id not initialized, {self.__class__.__name__} requires detections to be tracked"
             )
         metadata = image.video_metadata
+        zones = self.normalize_zone(zone)
         if metadata.video_identifier not in self._batch_of_polygon_zones:
-            if not isinstance(zone, list) or len(zone) < 3:
+            if len(zones) > 0 and not isinstance(zones[0], list) or len(zones[0]) < 3:
                 raise ValueError(
                     f"{self.__class__.__name__} requires zone to be a list containing more than 2 points"
                 )
             if any(
                 (not isinstance(e, list) and not isinstance(e, tuple)) or len(e) != 2
-                for e in zone
+                for e in self.flatten_list(zones)
             ):
                 raise ValueError(
                     f"{self.__class__.__name__} requires each point of zone to be a list containing exactly 2 coordinates"
                 )
             if any(
                 not isinstance(e[0], (int, float)) or not isinstance(e[1], (int, float))
-                for e in zone
+                for e in self.flatten_list(zones)
             ):
                 raise ValueError(
                     f"{self.__class__.__name__} requires each coordinate of zone to be a number"
                 )
-            self._batch_of_polygon_zones[metadata.video_identifier] = sv.PolygonZone(
+            self._batch_of_polygon_zones[metadata.video_identifier] = [sv.PolygonZone(
                 polygon=np.array(zone),
                 triggering_anchors=(sv.Position(triggering_anchor),),
-            )
-        polygon_zone = self._batch_of_polygon_zones[metadata.video_identifier]
+            ) for zone in zones]
+        polygon_zones = self._batch_of_polygon_zones[metadata.video_identifier]
         tracked_ids_in_zone = self._batch_of_tracked_ids_in_zone.setdefault(
             metadata.video_identifier, {}
         )
@@ -159,32 +169,33 @@ class TimeInZoneBlockV2(WorkflowBlock):
             ts_end = metadata.frame_number / metadata.fps
         else:
             ts_end = metadata.frame_timestamp.timestamp()
-        for i, is_in_zone, tracker_id in zip(
-            range(len(detections)),
-            polygon_zone.trigger(detections),
-            detections.tracker_id,
-        ):
-            if (
-                not is_in_zone
-                and tracker_id in tracked_ids_in_zone
-                and reset_out_of_zone_detections
+        for polygon_zone in polygon_zones:
+            for i, is_in_zone, tracker_id in zip(
+                range(len(detections)),
+                polygon_zone.trigger(detections),
+                detections.tracker_id,
             ):
-                del tracked_ids_in_zone[tracker_id]
-            if not is_in_zone and remove_out_of_zone_detections:
-                continue
+                if (
+                    not is_in_zone
+                    and tracker_id in tracked_ids_in_zone
+                    and reset_out_of_zone_detections
+                ):
+                    del tracked_ids_in_zone[tracker_id]
+                if not is_in_zone and remove_out_of_zone_detections:
+                    continue
 
-            # copy
-            detection = detections[i]
+                # copy
+                detection = detections[i]
 
-            detection[TIME_IN_ZONE_KEY_IN_SV_DETECTIONS] = np.array(
-                [0], dtype=np.float64
-            )
-            if is_in_zone:
-                ts_start = tracked_ids_in_zone.setdefault(tracker_id, ts_end)
                 detection[TIME_IN_ZONE_KEY_IN_SV_DETECTIONS] = np.array(
-                    [ts_end - ts_start], dtype=np.float64
+                    [0], dtype=np.float64
                 )
-            elif tracker_id in tracked_ids_in_zone:
-                del tracked_ids_in_zone[tracker_id]
-            result_detections.append(detection)
+                if is_in_zone:
+                    ts_start = tracked_ids_in_zone.setdefault(tracker_id, ts_end)
+                    detection[TIME_IN_ZONE_KEY_IN_SV_DETECTIONS] = np.array(
+                        [ts_end - ts_start], dtype=np.float64
+                    )
+                elif tracker_id in tracked_ids_in_zone:
+                    del tracked_ids_in_zone[tracker_id]
+                result_detections.append(detection)
         return {OUTPUT_KEY: sv.Detections.merge(result_detections)}
