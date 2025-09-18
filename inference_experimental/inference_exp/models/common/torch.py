bb2b38e48582b275b21635a5348fd2c5a92b7b9e
Paweł Pęczek <pawel@roboflow.com>
2025-09-18 22:53:14 +0200

    Add tests for new torch utils

--- inference_experimental/inference_exp/models/common/torch.py
+++ inference_experimental/inference_exp/models/common/torch.py
@@ -10,7 +10,7 @@ def generate_batch_chunks(
     n = input_batch.shape[0]
     for i in range(0, n, chunk_size):
         chunk = input_batch[i : i + chunk_size]
-        padding_size = chunk.shape[0] - chunk_size
+        padding_size = chunk_size - chunk.shape[0]
         if padding_size > 0:
             padding_shape = (padding_size,) + chunk.shape[1:]
             padding = torch.zeros(
