3245a3b10796d6423817709a59a0c819b4114998
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2025-09-30 16:29:24 +0200

    Limit rfdetr input resolution when loading through onnx

--- inference/models/rfdetr/rfdetr.py
+++ inference/models/rfdetr/rfdetr.py
@@ -15,6 +15,7 @@ from inference.core.env import (
     MAX_BATCH_SIZE,
     ONNXRUNTIME_EXECUTION_PROVIDERS,
     REQUIRED_ONNX_PROVIDERS,
+    RFDETR_ONNX_MAX_RESOLUTION,
     TENSORRT_CACHE_PATH,
     USE_PYTORCH_FOR_PREPROCESSING,
 )
@@ -385,6 +386,14 @@ class RFDETRObjectDetection(ObjectDetectionBaseOnnxRoboflowInferenceModel):
         """Initializes the ONNX model, setting up the inference session and other necessary properties."""
         logger.debug("Getting model artefacts")
         self.get_model_artifacts(**kwargs)
+        if self.environment.get("RESOLUTION") >= RFDETR_ONNX_MAX_RESOLUTION:
+            logger.error(
+                "NOT loading '%s' model, input resolution is '%s', ONNX max resolution limit set to '%s'",
+                self.endpoint,
+                self.environment.get("RESOLUTION"),
+                RFDETR_ONNX_MAX_RESOLUTION,
+            )
+            raise ModelArtefactError(f"Resolution too high for RFDETR")
         logger.debug("Creating inference session")
         if self.load_weights or not self.has_model_metadata:
             t1_session = perf_counter()
