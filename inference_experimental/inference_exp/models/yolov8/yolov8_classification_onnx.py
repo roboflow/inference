463a1b26bf414d31d05328338c00391ecdb2694f
Paweł Pęczek <pawel@roboflow.com>
2025-10-21 14:45:53 +0200

    Add implementation of yolov8-cls to inference-exp

--- inference_experimental/inference_exp/models/yolov8/yolov8_classification_onnx.py
+++ inference_experimental/inference_exp/models/yolov8/yolov8_classification_onnx.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import torch
-from inference_exp import Detections, KeyPoints, KeyPointsDetectionModel
+from inference_exp import ClassificationModel, ClassificationPrediction
 from inference_exp.configuration import DEFAULT_DEVICE
 from inference_exp.entities import ColorFormat
 from inference_exp.errors import (
@@ -11,6 +11,7 @@ from inference_exp.errors import (
     EnvironmentConfigurationError,
     MissingDependencyError,
 )
+from inference_exp.models.base.types import PreprocessedInputs
 from inference_exp.models.common.model_packages import get_model_package_contents
 from inference_exp.models.common.onnx import (
     run_session_with_batch_size_limit,
@@ -18,16 +19,9 @@ from inference_exp.models.common.onnx import (
 )
 from inference_exp.models.common.roboflow.model_packages import (
     InferenceConfig,
-    PreProcessingMetadata,
     ResizeMode,
     parse_class_names_file,
     parse_inference_config,
-    parse_key_points_metadata,
-)
-from inference_exp.models.common.roboflow.post_processing import (
-    post_process_nms_fused_model_output,
-    rescale_key_points_detections,
-    run_nms_for_key_points_detection,
 )
 from inference_exp.models.common.roboflow.pre_processing import (
     pre_process_network_input,
@@ -38,7 +32,7 @@ try:
     import onnxruntime
 except ImportError as import_error:
     raise MissingDependencyError(
-        message=f"Could not import YOLOv8 model with ONNX backend - this error means that some additional dependencies "
+        message=f"Could not import ResNet model with ONNX backend - this error means that some additional dependencies "
         f"are not installed in the environment. If you run the `inference-exp` library directly in your Python "
         f"program, make sure the following extras of the package are installed: \n"
         f"\t* `onnx-cpu` - when you wish to use library with CPU support only\n"
@@ -51,9 +45,7 @@ except ImportError as import_error:
     ) from import_error
 
 
-class YOLOv8ForKeyPointsDetectionOnnx(
-    KeyPointsDetectionModel[torch.Tensor, PreProcessingMetadata, torch.Tensor]
-):
+class YOLOv8ForClassificationOnnx(ClassificationModel[torch.Tensor, torch.Tensor]):
 
     @classmethod
     def from_pretrained(
@@ -63,7 +55,7 @@ class YOLOv8ForKeyPointsDetectionOnnx(
         default_onnx_trt_options: bool = True,
         device: torch.device = DEFAULT_DEVICE,
         **kwargs,
-    ) -> "YOLOv8ForKeyPointsDetectionOnnx":
+    ) -> "YOLOv8ForClassificationOnnx":
         if onnx_execution_providers is None:
             onnx_execution_providers = get_selected_onnx_execution_providers()
         if not onnx_execution_providers:
@@ -86,7 +78,6 @@ class YOLOv8ForKeyPointsDetectionOnnx(
                 "class_names.txt",
                 "inference_config.json",
                 "weights.onnx",
-                "keypoints_metadata.json",
             ],
         )
         class_names = parse_class_names_file(
@@ -101,31 +92,27 @@ class YOLOv8ForKeyPointsDetectionOnnx(
                 ResizeMode.LETTERBOX_REFLECT_EDGES,
             },
         )
-        if inference_config.post_processing.type != "nms":
+        if inference_config.post_processing.type != "softmax":
             raise CorruptedModelPackageError(
-                message="Expected NMS to be the post-processing",
+                message="Expected Softmax to be the post-processing",
                 help_url="https://todo",
             )
-        parsed_key_points_metadata, skeletons = parse_key_points_metadata(
-            key_points_metadata_path=model_package_content["keypoints_metadata.json"]
-        )
         session = onnxruntime.InferenceSession(
             path_or_bytes=model_package_content["weights.onnx"],
             providers=onnx_execution_providers,
         )
-        input_batch_size = session.get_inputs()[0].shape[0]
+        input_shape = session.get_inputs()[0].shape
+        input_batch_size = input_shape[0]
         if isinstance(input_batch_size, str):
             input_batch_size = None
         input_name = session.get_inputs()[0].name
         return cls(
             session=session,
             input_name=input_name,
-            class_names=class_names,
             inference_config=inference_config,
+            class_names=class_names,
             device=device,
             input_batch_size=input_batch_size,
-            parsed_key_points_metadata=parsed_key_points_metadata,
-            skeletons=skeletons,
         )
 
     def __init__(
@@ -136,36 +123,18 @@ class YOLOv8ForKeyPointsDetectionOnnx(
         class_names: List[str],
         device: torch.device,
         input_batch_size: Optional[int],
-        parsed_key_points_metadata: List[List[str]],
-        skeletons: List[List[Tuple[int, int]]],
     ):
         self._session = session
         self._input_name = input_name
         self._inference_config = inference_config
         self._class_names = class_names
-        self._skeletons = skeletons
         self._device = device
         self._input_batch_size = input_batch_size
         self._session_thread_lock = Lock()
-        self._parsed_key_points_metadata = parsed_key_points_metadata
-        self._key_points_classes_for_instances = torch.tensor(
-            [len(e) for e in self._parsed_key_points_metadata], device=device
-        )
-        self._key_points_slots_in_prediction = max(
-            len(e) for e in parsed_key_points_metadata
-        )
 
     @property
     def class_names(self) -> List[str]:
         return self._class_names
-
-    @property
-    def key_points_classes(self) -> List[List[str]]:
-        return self._parsed_key_points_metadata
-
-    @property
-    def skeletons(self) -> List[List[Tuple[int, int]]]:
-        return self._skeletons
 
     def pre_process(
         self,
@@ -173,7 +142,7 @@ class YOLOv8ForKeyPointsDetectionOnnx(
         input_color_format: Optional[ColorFormat] = None,
         image_size: Optional[Tuple[int, int]] = None,
         **kwargs,
-    ) -> Tuple[torch.Tensor, List[PreProcessingMetadata]]:
+    ) -> torch.Tensor:
         return pre_process_network_input(
             images=images,
             image_pre_processing=self._inference_config.image_pre_processing,
@@ -181,9 +150,11 @@ class YOLOv8ForKeyPointsDetectionOnnx(
             target_device=self._device,
             input_color_format=input_color_format,
             image_size_wh=image_size,
-        )
+        )[0]
 
-    def forward(self, pre_processed_images: torch.Tensor, **kwargs) -> torch.Tensor:
+    def forward(
+        self, pre_processed_images: PreprocessedInputs, **kwargs
+    ) -> torch.Tensor:
         with self._session_thread_lock:
             return run_session_with_batch_size_limit(
                 session=self._session,
@@ -195,66 +166,13 @@ class YOLOv8ForKeyPointsDetectionOnnx(
     def post_process(
         self,
         model_results: torch.Tensor,
-        pre_processing_meta: List[PreProcessingMetadata],
-        conf_thresh: float = 0.25,
-        iou_thresh: float = 0.45,
-        max_detections: int = 100,
-        class_agnostic: bool = False,
-        key_points_threshold: float = 0.3,
         **kwargs,
-    ) -> Tuple[List[KeyPoints], Optional[List[Detections]]]:
+    ) -> ClassificationPrediction:
         if self._inference_config.post_processing.fused:
-            nms_results = post_process_nms_fused_model_output(
-                output=model_results, conf_thresh=conf_thresh
-            )
+            confidence = model_results
         else:
-            nms_results = run_nms_for_key_points_detection(
-                output=model_results,
-                num_classes=len(self._class_names),
-                key_points_slots_in_prediction=self._key_points_slots_in_prediction,
-                conf_thresh=conf_thresh,
-                iou_thresh=iou_thresh,
-                max_detections=max_detections,
-                class_agnostic=class_agnostic,
-            )
-        rescaled_results = rescale_key_points_detections(
-            detections=nms_results,
-            images_metadata=pre_processing_meta,
-            num_classes=len(self._class_names),
-            key_points_slots_in_prediction=self._key_points_slots_in_prediction,
+            confidence = torch.nn.functional.softmax(model_results, dim=-1)
+        return ClassificationPrediction(
+            class_id=confidence.argmax(dim=-1),
+            confidence=confidence,
         )
-        detections, all_key_points = [], []
-        for result in rescaled_results:
-            class_id = result[:, 5].int()
-            detections.append(
-                Detections(
-                    xyxy=result[:, :4].round().int(),
-                    class_id=class_id,
-                    confidence=result[:, 4],
-                )
-            )
-            key_points_reshaped = result[:, 6:].view(
-                result.shape[0], self._key_points_slots_in_prediction, 3
-            )
-            xy = key_points_reshaped[:, :, :2]
-            confidence = key_points_reshaped[:, :, 2]
-            key_points_classes_for_instance_class = (
-                (self._key_points_classes_for_instances[class_id])
-                .unsqueeze(1)
-                .to(device=result.device)
-            )
-            instances_class_mask = (
-                torch.arange(self._key_points_slots_in_prediction, device=result.device)
-                .unsqueeze(0)
-                .repeat(result.shape[0], 1)
-                < key_points_classes_for_instance_class
-            )
-
-            confidence_mask = confidence < key_points_threshold
-            mask = instances_class_mask & confidence_mask
-            xy[mask] = 0.0
-            confidence[mask] = 0.0
-            all_key_points.append(
-                KeyPoints(xy=xy.round().int(), class_id=class_id, confidence=confidence)
-            )
-        return all_key_points, detections
