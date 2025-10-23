612a2bbd727143a89126aa43ab3b4e01d824721c
Thomas Hansen <thomas.hansen@gmail.com>
2025-10-23 17:18:05 -0500

    move exp adapter to its own file

--- inference/models/yolov8/__init__.py
+++ inference/models/yolov8/__init__.py
@@ -7,7 +7,7 @@ from inference.models.yolov8.yolov8_keypoints_detection import YOLOv8KeypointsDe
 from inference.core.env import USE_INFERENCE_EXP_MODELS
 
 if USE_INFERENCE_EXP_MODELS:
-    from inference.models.yolov8.yolov8_object_detection_ext import (
+    from inference.models.yolov8.yolov8_object_detection_exp import (
         Yolo8ODExperimentalModel as YOLOv8ObjectDetection,
     )
 else:
---
+++ inference/models/yolov8/yolov8_object_detection_exp.py
@@ -0,0 +1,5 @@
+from inference.core.models.exp_adapter import InferenceExpObjectDetectionModelAdapter
+
+
+class Yolo8ODExperimentalModel(InferenceExpObjectDetectionModelAdapter):
+    pass
