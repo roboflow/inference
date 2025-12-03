a0d63cdaee24fc6baaa56304ac64072eaa7c19d6
Paweł Pęczek <pawel@roboflow.com>
2025-12-03 14:37:23 +0100

    Add tests for Depth Anything V2 and remove faulty E2E tests of other models

--- inference_experimental/tests/e2e_platform_tests/test_florence2_e2e.py
+++ inference_experimental/tests/e2e_platform_tests/test_florence2_e2e.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-import torch
 from inference_exp import AutoModel
 
 
@@ -24,24 +23,6 @@ def test_florence2_base_model(dog_image_numpy: np.ndarray):
 def test_florence2_base_model(dog_image_numpy: np.ndarray):
     # GIVEN
     model = AutoModel.from_pretrained("florence-2-large")
-
-    # WHEN
-    captions = model.caption_image(dog_image_numpy)
-
-    # THEN
-    assert isinstance(captions, list)
-    assert len(captions) == 1
-    assert isinstance(captions[0], str)
-    assert len(captions[0]) > 0
-
-
-@pytest.mark.e2e_model_inference
-@pytest.mark.gpu_only
-def test_florence2_lora_model(
-    dog_image_numpy: np.ndarray, dog_image_torch: torch.Tensor
-):
-    # GIVEN
-    model = AutoModel.from_pretrained("florence-2-lora-test")
 
     # WHEN
     captions = model.caption_image(dog_image_numpy)
