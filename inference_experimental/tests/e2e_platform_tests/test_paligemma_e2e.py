35feb60be9aaae9bb96414933876b3165770119f
Paweł Pęczek <pawel@roboflow.com>
2025-12-03 13:52:42 +0100

    Add missing models

--- inference_experimental/tests/e2e_platform_tests/test_paligemma_e2e.py
+++ inference_experimental/tests/e2e_platform_tests/test_paligemma_e2e.py
@@ -4,10 +4,25 @@ from inference_exp import AutoModel
 
 
 @pytest.mark.e2e_model_inference
-@pytest.mark.gpu_only
 def test_paligemma_base_model(dog_image_numpy: np.ndarray):
     # GIVEN
     model = AutoModel.from_pretrained("paligemma2-3b-pt-224")
+
+    # WHEN
+    captions = model.prompt(images=dog_image_numpy, prompt="What is in the image?")
+
+    # THEN
+    assert isinstance(captions, list)
+    assert len(captions) == 1
+    assert isinstance(captions[0], str)
+    assert len(captions[0]) > 0
+
+
+@pytest.mark.e2e_model_inference
+@pytest.mark.gpu_only
+def test_paligemma_3b_mix_244_model(dog_image_numpy: np.ndarray):
+    # GIVEN
+    model = AutoModel.from_pretrained("paligemma/3b-mix-224")
 
     # WHEN
     captions = model.prompt(images=dog_image_numpy, prompt="What is in the image?")
