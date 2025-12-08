e02270ca64e86970ea1d0cb81bb7eea910c73621
Matvezy <mpopov@trinity.edu>
2025-12-08 09:48:55 +0000

    add license

--- inference/core/workflows/core_steps/loader.py
+++ inference/core/workflows/core_steps/loader.py
@@ -243,9 +243,6 @@ from inference.core.workflows.core_steps.models.foundation.segment_anything3.v1 
 from inference.core.workflows.core_steps.models.foundation.segment_anything3.v2 import (
     SegmentAnything3BlockV2,
 )
-from inference.core.workflows.core_steps.models.foundation.segment_anything3_3d.v1 import (
-    SegmentAnything3_3D_ObjectsBlockV1,
-)
 from inference.core.workflows.core_steps.models.foundation.smolvlm.v1 import (
     SmolVLM2BlockV1,
 )
