abf49c1de2cfb6c012ffd78ed44a43449eea67ff
Matvezy <mpopov@trinity.edu>
2025-12-13 09:43:01 +0000

    add aliases

--- inference_sdk/http/utils/aliases.py
+++ inference_sdk/http/utils/aliases.py
@@ -34,6 +34,13 @@ QWEN_ALIASES = {
     "qwen25-vl-7b": "qwen-pretrains/1",
 }
 
+CLASSIFICATION_ALIASES = {
+    "resent18": "classifiers/1",
+    "resent34": "classifiers/2",
+    "resent50": "classifiers/3",
+    "resent101": "classifiers/4",
+}
+
 REGISTERED_ALIASES = {
     "yolov8n-640": "coco/3",
     "yolov8n-1280": "coco/9",
@@ -92,6 +99,7 @@ REGISTERED_ALIASES = {
     **YOLOV11_ALIASES,
     **QWEN_ALIASES,
     **RFDETR_ALIASES,
+    **CLASSIFICATION_ALIASES,
 }
 
 OCR_ENDPOINTS = {
