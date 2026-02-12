8f1f31f5eda8cd0fbd9dd42f1609dbd43db2d6f9
Paweł Pęczek <pawel@roboflow.com>
2026-02-12 23:36:52 +0100

    Fix issue with auto-negotiation cache misses

--- inference_models/inference_models/models/auto_loaders/core.py
+++ inference_models/inference_models/models/auto_loaders/core.py
@@ -8,7 +8,6 @@ from functools import partial
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
 
 import torch
-from argon2 import PasswordHasher
 from filelock import FileLock
 from rich.console import Console
 from rich.text import Text
@@ -716,16 +715,11 @@ class AutoModel:
             # that still may end up with ambiguous behavior - probably the solution would be
             # to require prefix like file://... to denote the intent of loading model from local
             # drive?
-            if api_key is not None:
-                password_hashed = PasswordHasher()
-                api_key_hash = password_hashed.hash(api_key)
-            else:
-                api_key_hash = api_key
             auto_negotiation_hash = hash_dict_content(
                 content={
                     "provider": weights_provider,
                     "model_id": model_id_or_path,
-                    "api_key": api_key_hash,
+                    "api_key": api_key,
                     "requested_model_package_id": model_package_id,
                     "requested_backends": backend,
                     "requested_batch_size": batch_size,
