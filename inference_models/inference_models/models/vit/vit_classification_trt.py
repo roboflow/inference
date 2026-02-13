b8623afa88b96c07d55c1ba556c38b2fafc96d66
Paweł Pęczek <pawel@roboflow.com>
2026-02-13 01:08:27 +0100

    Fix issue with fit-longer-edge preprocessing for vit

--- inference_models/inference_models/models/vit/vit_classification_trt.py
+++ inference_models/inference_models/models/vit/vit_classification_trt.py
@@ -102,12 +102,12 @@ class VITForClassificationTRT(ClassificationModel[torch.Tensor, torch.Tensor]):
             },
             implicit_resize_mode_substitutions={
                 ResizeMode.FIT_LONGER_EDGE: (
-                    ResizeMode.LETTERBOX,
-                    0,
+                    ResizeMode.STRETCH_TO,
+                    None,
                     "VIT Classification model running with TRT backend was trained with "
                     "`fit-longer-edge` input resize mode. This transform cannot be applied properly for "
-                    "models with input dimensions fixed during weights export. To ensure interoperability, `letterbox` "
-                    "resize mode with black edges will be used instead. If model was trained on Roboflow platform, "
+                    "models with input dimensions fixed during weights export. To ensure interoperability, `stretch` "
+                    "resize mode will be used instead. If model was trained on Roboflow platform, "
                     "we recommend using preprocessing method different that `fit-longer-edge`.",
                 )
             },
