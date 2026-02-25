b1ec26052d66dc66dc2098ff27b78d79cebaf362
Rafel Bennasar Crespi <253519461+rafel-roboflow@users.noreply.github.com>
2026-02-25 17:26:15 +0100

    fixed wrong import

--- inference/core/interfaces/http/http_api.py
+++ inference/core/interfaces/http/http_api.py
@@ -192,7 +192,6 @@ from inference.core.exceptions import (
     RoboflowAPINotNotFoundError,
     WebRTCConfigurationError,
     WorkspaceLoadError,
-    WorkflowError,
 )
 from inference.core.interfaces.base import BaseInterface
 from inference.core.interfaces.http.dependencies import (
@@ -248,7 +247,7 @@ from inference.core.roboflow_api import (
 from inference.core.utils.container import is_docker_socket_mounted
 from inference.core.utils.notebooks import start_notebook
 from inference.core.workflows.core_steps.common.entities import StepExecutionMode
-from inference.core.workflows.errors import WorkflowSyntaxError
+from inference.core.workflows.errors import WorkflowError, WorkflowSyntaxError
 from inference.core.workflows.execution_engine.core import (
     ExecutionEngine,
     get_available_versions,
