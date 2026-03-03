574e684880b0d4cca83620b5ee38104dbde856c5
Kai Christensen <matthew.kai.christensen@gmail.com>
2026-03-03 18:10:53 +0000

    set this feature flag to false by default

--- inference_models/inference_models/configuration.py
+++ inference_models/inference_models/configuration.py
@@ -87,7 +87,7 @@ ALLOW_LOCAL_STORAGE_ACCESS_FOR_REFERENCE_DATA = os.getenv(
 
 USE_CUDA_GRAPHS_FOR_TRT_BACKEND = get_boolean_from_env(
     variable_name="USE_CUDA_GRAPHS_FOR_TRT_BACKEND",
-    default=True,
+    default=False,
 )
 
 # General model parameters defaults
