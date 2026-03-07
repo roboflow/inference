e314a621b4850c1542d5996efcbf7c3d67cd5ee5
Matvezy <mpopov@trinity.edu>
2026-03-07 10:11:19 +0000

    drop redundant files

--- doctr_weights.py
+++
@@ -1,9 +0,0 @@
-import torch
-from doctr.models import db_resnet50, crnn_vgg16_bn
-
-# Get v2 weights (doctr 1.0+ must be installed)
-det_model = db_resnet50(pretrained=True)
-torch.save(det_model.state_dict(), "db_resnet50_v2.pt")
-
-rec_model = crnn_vgg16_bn(pretrained=True)
-torch.save(rec_model.state_dict(), "crnn_vgg16_bn_v2.pt")
