b45528da71c7d164f15c04a2c837ab9ff80d75c3
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2026-03-26 15:55:11 +0100

    When running on ARM webrtc worker was not shutting down cleanly due to onnxruntime-gpu coredumping while garbage-collected

--- inference/core/interfaces/webrtc_worker/cpu.py
+++ inference/core/interfaces/webrtc_worker/cpu.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from multiprocessing.connection import Connection
 
 from inference.core import logger
@@ -19,10 +20,18 @@ def rtc_peer_connection_process(
         answer_conn.send(obj)
         answer_conn.close()
 
-    asyncio.run(
-        init_rtc_peer_connection_with_loop(
-            webrtc_request=webrtc_request,
-            send_answer=send_answer,
+    try:
+        asyncio.run(
+            init_rtc_peer_connection_with_loop(
+                webrtc_request=webrtc_request,
+                send_answer=send_answer,
+            )
         )
-    )
-    logger.info("WebRTC process terminated")
+        logger.info("WebRTC process terminated")
+    except Exception:
+        logger.exception("WebRTC worker process failed")
+        answer_conn.close()
+    finally:
+        # Skip Python interpreter cleanup to avoid onnxruntime crash
+        # on ARM64/Jetson during garbage collection at exit
+        os._exit(0)
