655e5bb85c359f0eb835d56c22db144a617cf0b3
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2026-03-26 19:58:53 +0100

    Fix/clean webrtc worker shutdown on arm (#2169)
    * When running on ARM webrtc worker was not shutting down cleanly due to onnxruntime-gpu coredumping while garbage-collected
    
    * pass watchdog to local CPU webrtc worker

--- inference/core/interfaces/webrtc_worker/cpu.py
+++ inference/core/interfaces/webrtc_worker/cpu.py
@@ -1,11 +1,14 @@
 import asyncio
+import os
 from multiprocessing.connection import Connection
 
 from inference.core import logger
+from inference.core.env import WEBRTC_MODAL_WATCHDOG_TIMEMOUT
 from inference.core.interfaces.webrtc_worker.entities import (
     WebRTCWorkerRequest,
     WebRTCWorkerResult,
 )
+from inference.core.interfaces.webrtc_worker.watchdog import Watchdog
 from inference.core.interfaces.webrtc_worker.webrtc import (
     init_rtc_peer_connection_with_loop,
 )
@@ -19,10 +22,24 @@ def rtc_peer_connection_process(
         answer_conn.send(obj)
         answer_conn.close()
 
-    asyncio.run(
-        init_rtc_peer_connection_with_loop(
-            webrtc_request=webrtc_request,
-            send_answer=send_answer,
-        )
+    watchdog = Watchdog(
+        api_key=webrtc_request.api_key,
+        timeout_seconds=WEBRTC_MODAL_WATCHDOG_TIMEMOUT,
     )
-    logger.info("WebRTC process terminated")
+
+    try:
+        asyncio.run(
+            init_rtc_peer_connection_with_loop(
+                webrtc_request=webrtc_request,
+                send_answer=send_answer,
+                heartbeat_callback=watchdog.heartbeat,
+            )
+        )
+        logger.info("WebRTC process terminated")
+    except Exception:
+        logger.exception("WebRTC worker process failed")
+        answer_conn.close()
+    finally:
+        # Skip Python interpreter cleanup to avoid onnxruntime crash
+        # on ARM64/Jetson during garbage collection at exit
+        os._exit(0)
