534b2c62e84ccea61699c3930d3e12f37e24707a
Lee Clement <lee@roboflow.com>
2026-04-17 11:33:39 -0230

    use pydantic native ge/le validation instead of annotated_types

--- inference_models/inference_models/entities.py
+++ inference_models/inference_models/entities.py
@@ -1,8 +1,8 @@
 from collections import namedtuple
 from typing import Annotated, Literal, Union
 
-from annotated_types import Ge, Le
+from pydantic import Field
 
 ImageDimensions = namedtuple("ImageDimensions", ["height", "width"])
 ColorFormat = Literal["rgb", "bgr"]
-Confidence = Union[Annotated[float, Ge(0), Le(1)], Literal["best", "default"]]
+Confidence = Union[Annotated[float, Field(ge=0, le=1)], Literal["best", "default"]]
