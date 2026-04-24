986ed1158a86796405ded32c42e125015c915733
Paweł Pęczek <pawel@roboflow.com>
2026-04-24 22:17:46 +0200

    Make linters happy

--- inference/models/owlv2/owlv2_inference_models.py
+++ inference/models/owlv2/owlv2_inference_models.py
@@ -42,7 +42,10 @@ from inference_models.models.owlv2.entities import (
     ReferenceBoundingBox,
     ReferenceExample,
 )
-from inference_models.models.owlv2.owlv2_hf import OWLv2HF, monkey_patch_vision_encoder_before_compilation
+from inference_models.models.owlv2.owlv2_hf import (
+    OWLv2HF,
+    monkey_patch_vision_encoder_before_compilation,
+)
 
 PRELOADED_HF_MODELS = {}
 
@@ -95,7 +98,9 @@ class Owlv2AdapterSingleton:
             if OWLV2_COMPILE_MODEL:
                 logger.info("Compiling OWLv2 model %s", huggingface_id)
                 torch._dynamo.config.suppress_errors = True
-                model._model = monkey_patch_vision_encoder_before_compilation(model._model)
+                model._model = monkey_patch_vision_encoder_before_compilation(
+                    model._model
+                )
                 model._model.owlv2.vision_model = torch.compile(
                     model._model.owlv2.vision_model
                 )
