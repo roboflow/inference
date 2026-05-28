03ce5cc89848b99bf9c9a6cf3bc2ef0e3f65bb4d
Grzegorz Klimaszewski <166530809+grzegorz-roboflow@users.noreply.github.com>
2026-05-28 21:16:20 +0200

    add SAM3Torch to _TASK_CONFIGS; wire SAM3 image+prompts actions

--- inference_server/inference_server/handlers/interactive_instance_segmentation/description.py
+++ inference_server/inference_server/handlers/interactive_instance_segmentation/description.py
@@ -9,12 +9,15 @@ from inference_server.handlers.interactive_instance_segmentation.input_parser im
     parse_interactive_instance_segmentation_input,
 )
 from inference_server.handlers.interactive_instance_segmentation.introspection import (
+    get_sam3_text_prompts_interface,
+    get_sam3_visual_prompts_interface,
     get_sam_embeddings_image_only_interface,
     get_sam_text_image_only_interface,
     get_sam_text_prompt_interface,
 )
 from inference_server.handlers.interactive_instance_segmentation.output_serializer import (
     serialize_sam_embeddings,
+    serialize_sam_segmentation,
     serialize_sam_text,
 )
 
@@ -41,7 +44,32 @@ _TEXT_PROMPT = ModelHandlerDescription(
 )
 
 
+_SAM3_VISUAL = ModelHandlerDescription(
+    input_parser=parse_interactive_instance_segmentation_input,
+    handler=handle_interactive_instance_segmentation,
+    output_serializer=serialize_sam_segmentation,
+    interface_provider=get_sam3_visual_prompts_interface,
+)
+
+_SAM3_TEXT = ModelHandlerDescription(
+    input_parser=parse_interactive_instance_segmentation_input,
+    handler=handle_interactive_instance_segmentation,
+    output_serializer=serialize_sam_segmentation,
+    interface_provider=get_sam3_text_prompts_interface,
+)
+
+
 _register("interactive-instance-segmentation", "embed", _EMBEDDINGS_IMAGE_ONLY)
 _register("interactive-instance-segmentation", "embed_images", _EMBEDDINGS_IMAGE_ONLY)
 _register("interactive-instance-segmentation", "prompt", _TEXT_PROMPT)
 _register("interactive-instance-segmentation", "track", _TEXT_IMAGE_ONLY)
+_register(
+    "interactive-instance-segmentation",
+    "segment_with_visual_prompts",
+    _SAM3_VISUAL,
+)
+_register(
+    "interactive-instance-segmentation",
+    "segment_with_text_prompts",
+    _SAM3_TEXT,
+)
