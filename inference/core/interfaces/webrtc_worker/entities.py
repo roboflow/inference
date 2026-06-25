e6130e9f443ddba6553dc393a8c6287a94d7aa2b
Rafel Bennasar Crespi <253519461+rafel-roboflow@users.noreply.github.com>
2026-06-25 17:27:37 +0200

    clean up

--- inference/core/interfaces/webrtc_worker/entities.py
+++ inference/core/interfaces/webrtc_worker/entities.py
@@ -52,6 +52,8 @@ class WebRTCWorkerRequest(BaseModel):
     requested_plan: Optional[str] = "webrtc-gpu-small"
     # TODO: replaced with requested_plan
     requested_gpu: Optional[str] = None
+    # must be valid region: https://modal.com/docs/guide/region-selection#region-options
+    requested_region: Optional[str] = None
 
     workspace_id: Optional[str] = None
     session_id: Optional[str] = None
