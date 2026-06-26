3d13270d43c3467a1644de28c1913b458e3052cf
Rafel Bennasar Crespi <253519461+rafel-roboflow@users.noreply.github.com>
2026-06-26 14:31:19 +0200

    only cancel and not terminating containers

--- inference/core/interfaces/webrtc_worker/modal.py
+++ inference/core/interfaces/webrtc_worker/modal.py
@@ -719,8 +719,8 @@ if modal is not None:
                     q.get(block=True, timeout=WEBRTC_MODAL_RESPONSE_TIMEOUT)
                 )
             except Empty:
-                logger.error("Modal function call timed out, terminating containers")
-                function_call.cancel(terminate_containers=True)
+                logger.error("Modal function call timed out, cancelling function call")
+                function_call.cancel()
                 raise RoboflowAPITimeoutError("Modal function call timed out")
             except Exception as exc:
                 logger.error(exc)
