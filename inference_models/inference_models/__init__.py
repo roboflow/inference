765f979f2d1912e7e0e5d1f9df4e0221bce0d02d
Paweł Pęczek <pawel@roboflow.com>
2026-08-21 12:51:12 +0200

    Extend public interface

--- inference_models/inference_models/__init__.py
+++ inference_models/inference_models/__init__.py
@@ -73,3 +73,10 @@ from inference_models.models.base.object_detection import (
 from inference_models.models.base.semantic_segmentation import SemanticSegmentationModel
 from inference_models.models.base.types import InstancesRLEMasks
 from inference_models.weights_providers.entities import Quantization
+from inference_models.weights_providers.offline_registry import (
+    OfflinePackagePresence,
+    OfflineArtefactStatus,
+    OfflinePackageStatus,
+    OfflineModelStatus,
+    OfflineArtefactVerification
+)
